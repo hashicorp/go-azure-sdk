e354ee043a90c2196e94e30afae5b4c5b5514a2d
hc-github-team-tf-azure <>
2022-07-22 09:03:54 +0000

    Updating based on 119288c

--- resource-manager/chaosstudio/2022-07-01-preview/capabilities/version.go
+++ resource-manager/chaosstudio/2022-07-01-preview/capabilities/version.go
@@ -1,12 +1,12 @@
-package targets
+package capabilities
 
 import "fmt"
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-const defaultApiVersion = "2021-09-15-preview"
+const defaultApiVersion = "2022-07-01-preview"
 
 func userAgent() string {
-	return fmt.Sprintf("hashicorp/go-azure-sdk/targets/%s", defaultApiVersion)
+	return fmt.Sprintf("hashicorp/go-azure-sdk/capabilities/%s", defaultApiVersion)
 }
--- resource-manager/chaosstudio/2022-07-01-preview/experiments/method_createorupdate_autorest.go
+++ resource-manager/chaosstudio/2022-07-01-preview/experiments/method_createorupdate_autorest.go
@@ -2,20 +2,18 @@ package experiments
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/azure"
-	"github.com/hashicorp/go-azure-helpers/polling"
 )
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
 type CreateOrUpdateOperationResponse struct {
-	Poller       polling.LongRunningPoller
 	HttpResponse *http.Response
+	Model        *Experiment
 }
 
 // CreateOrUpdate ...
@@ -26,27 +24,19 @@ func (c ExperimentsClient) CreateOrUpdate(ctx context.Context, id ExperimentId, 
 		return
 	}
 
-	result, err = c.senderForCreateOrUpdate(ctx, req)
+	result.HttpResponse, err = c.Client.Send(req, azure.DoRetryWithRegistration(c.Client))
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "experiments.ExperimentsClient", "CreateOrUpdate", result.HttpResponse, "Failure sending request")
 		return
 	}
 
-	return
-}
-
-// CreateOrUpdateThenPoll performs CreateOrUpdate then polls until it's completed
-func (c ExperimentsClient) CreateOrUpdateThenPoll(ctx context.Context, id ExperimentId, input Experiment) error {
-	result, err := c.CreateOrUpdate(ctx, id, input)
+	result, err = c.responderForCreateOrUpdate(result.HttpResponse)
 	if err != nil {
-		return fmt.Errorf("performing CreateOrUpdate: %+v", err)
+		err = autorest.NewErrorWithError(err, "experiments.ExperimentsClient", "CreateOrUpdate", result.HttpResponse, "Failure responding to request")
+		return
 	}
 
-	if err := result.Poller.PollUntilDone(); err != nil {
-		return fmt.Errorf("polling after CreateOrUpdate: %+v", err)
-	}
-
-	return nil
+	return
 }
 
 // preparerForCreateOrUpdate prepares the CreateOrUpdate request.
@@ -65,15 +55,14 @@ func (c ExperimentsClient) preparerForCreateOrUpdate(ctx context.Context, id Exp
 	return preparer.Prepare((&http.Request{}).WithContext(ctx))
 }
 
-// senderForCreateOrUpdate sends the CreateOrUpdate request. The method will close the
-// http.Response Body if it receives an error.
-func (c ExperimentsClient) senderForCreateOrUpdate(ctx context.Context, req *http.Request) (future CreateOrUpdateOperationResponse, err error) {
-	var resp *http.Response
-	resp, err = c.Client.Send(req, azure.DoRetryWithRegistration(c.Client))
-	if err != nil {
-		return
-	}
-
-	future.Poller, err = polling.NewPollerFromResponse(ctx, resp, c.Client, req.Method)
+// responderForCreateOrUpdate handles the response to the CreateOrUpdate request. The method always
+// closes the http.Response Body.
+func (c ExperimentsClient) responderForCreateOrUpdate(resp *http.Response) (result CreateOrUpdateOperationResponse, err error) {
+	err = autorest.Respond(
+		resp,
+		azure.WithErrorUnlessStatusCode(http.StatusOK),
+		autorest.ByUnmarshallingJSON(&result.Model),
+		autorest.ByClosing())
+	result.HttpResponse = resp
 	return
 }
