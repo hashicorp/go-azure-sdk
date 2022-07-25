ea2a32d2b1a2590b312eb3adb43da033dc44ff0d
hc-github-team-tf-azure <>
2022-07-25 10:00:06 +0000

    Updating based on 0265e24

--- resource-manager/eventhub/2018-01-01-preview/eventhubsclustersavailableclusterregions/model_availableclusterslist.go
+++
@@ -1,8 +0,0 @@
-package eventhubsclustersavailableclusterregions
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type AvailableClustersList struct {
-	Value *[]AvailableCluster `json:"value,omitempty"`
-}
