cbb4eceabb50f77b7604b3da6c000a6c732f54f7
hc-github-team-tf-azure <>
2022-07-28 08:48:37 +0000

    Updating based on 1ed6371

---
+++ resource-manager/dashboard/2022-08-01/client.go
@@ -0,0 +1,32 @@
+package v2022_08_01
+
+import (
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/dashboard/2022-08-01/grafanaresource"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/dashboard/2022-08-01/privateendpointconnection"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/dashboard/2022-08-01/privatelinkresource"
+)
+
+type Client struct {
+	GrafanaResource           *grafanaresource.GrafanaResourceClient
+	PrivateEndpointConnection *privateendpointconnection.PrivateEndpointConnectionClient
+	PrivateLinkResource       *privatelinkresource.PrivateLinkResourceClient
+}
+
+func NewClientWithBaseURI(endpoint string, configureAuthFunc func(c *autorest.Client)) Client {
+
+	grafanaResourceClient := grafanaresource.NewGrafanaResourceClientWithBaseURI(endpoint)
+	configureAuthFunc(&grafanaResourceClient.Client)
+
+	privateEndpointConnectionClient := privateendpointconnection.NewPrivateEndpointConnectionClientWithBaseURI(endpoint)
+	configureAuthFunc(&privateEndpointConnectionClient.Client)
+
+	privateLinkResourceClient := privatelinkresource.NewPrivateLinkResourceClientWithBaseURI(endpoint)
+	configureAuthFunc(&privateLinkResourceClient.Client)
+
+	return Client{
+		GrafanaResource:           &grafanaResourceClient,
+		PrivateEndpointConnection: &privateEndpointConnectionClient,
+		PrivateLinkResource:       &privateLinkResourceClient,
+	}
+}
