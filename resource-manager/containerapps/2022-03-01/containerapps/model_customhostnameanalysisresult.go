06e658d20c434de3d135f8e178301c5d93eca4b8
hc-github-team-tf-azure <>
2022-08-09 09:03:59 +0000

    Updating based on 0aca378

--- resource-manager/containerapps/2022-03-01/containerapps/model_customhostnameanalysisresult.go
+++ resource-manager/containerapps/2022-03-01/containerapps/model_customhostnameanalysisresult.go
@@ -4,15 +4,15 @@ package containerapps
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
 type CustomHostnameAnalysisResult struct {
-	ARecords                            *[]string                  `json:"aRecords,omitempty"`
-	AlternateCNameRecords               *[]string                  `json:"alternateCNameRecords,omitempty"`
-	AlternateTxtRecords                 *[]string                  `json:"alternateTxtRecords,omitempty"`
-	CNameRecords                        *[]string                  `json:"cNameRecords,omitempty"`
-	ConflictingContainerAppResourceId   *string                    `json:"conflictingContainerAppResourceId,omitempty"`
-	CustomDomainVerificationFailureInfo *DefaultErrorResponse      `json:"customDomainVerificationFailureInfo,omitempty"`
-	CustomDomainVerificationTest        *DnsVerificationTestResult `json:"customDomainVerificationTest,omitempty"`
-	HasConflictOnManagedEnvironment     *bool                      `json:"hasConflictOnManagedEnvironment,omitempty"`
-	HostName                            *string                    `json:"hostName,omitempty"`
-	IsHostnameAlreadyVerified           *bool                      `json:"isHostnameAlreadyVerified,omitempty"`
-	TxtRecords                          *[]string                  `json:"txtRecords,omitempty"`
+	ARecords                            *[]string                                                        `json:"aRecords,omitempty"`
+	AlternateCNameRecords               *[]string                                                        `json:"alternateCNameRecords,omitempty"`
+	AlternateTxtRecords                 *[]string                                                        `json:"alternateTxtRecords,omitempty"`
+	CNameRecords                        *[]string                                                        `json:"cNameRecords,omitempty"`
+	ConflictingContainerAppResourceId   *string                                                          `json:"conflictingContainerAppResourceId,omitempty"`
+	CustomDomainVerificationFailureInfo *CustomHostnameAnalysisResultCustomDomainVerificationFailureInfo `json:"customDomainVerificationFailureInfo,omitempty"`
+	CustomDomainVerificationTest        *DnsVerificationTestResult                                       `json:"customDomainVerificationTest,omitempty"`
+	HasConflictOnManagedEnvironment     *bool                                                            `json:"hasConflictOnManagedEnvironment,omitempty"`
+	HostName                            *string                                                          `json:"hostName,omitempty"`
+	IsHostnameAlreadyVerified           *bool                                                            `json:"isHostnameAlreadyVerified,omitempty"`
+	TxtRecords                          *[]string                                                        `json:"txtRecords,omitempty"`
 }
