f179d9b92b4467c5b440bf2edf04db50b815d8f0
hc-github-team-tf-azure <>
2022-08-09 08:09:10 +0000

    Updating based on 2ec985b

---
+++ resource-manager/deviceprovisioningservices/2022-02-05/client.go
@@ -0,0 +1,26 @@
+package v2022_02_05
+
+import (
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/deviceprovisioningservices/2022-02-05/dpscertificate"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/deviceprovisioningservices/2022-02-05/iotdpsresource"
+)
+
+type Client struct {
+	DpsCertificate *dpscertificate.DpsCertificateClient
+	IotDpsResource *iotdpsresource.IotDpsResourceClient
+}
+
+func NewClientWithBaseURI(endpoint string, configureAuthFunc func(c *autorest.Client)) Client {
+
+	dpsCertificateClient := dpscertificate.NewDpsCertificateClientWithBaseURI(endpoint)
+	configureAuthFunc(&dpsCertificateClient.Client)
+
+	iotDpsResourceClient := iotdpsresource.NewIotDpsResourceClientWithBaseURI(endpoint)
+	configureAuthFunc(&iotDpsResourceClient.Client)
+
+	return Client{
+		DpsCertificate: &dpsCertificateClient,
+		IotDpsResource: &iotDpsResourceClient,
+	}
+}
