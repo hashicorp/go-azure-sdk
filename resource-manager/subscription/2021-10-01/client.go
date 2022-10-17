0dc31d514fe616f90193e1150d7bf0966f1695ee
hc-github-team-tf-azure <>
2022-10-17 12:35:31 +0000

    Updating based on 834e89a5

---
+++ resource-manager/subscription/2021-10-01/client.go
@@ -0,0 +1,20 @@
+package v2021_10_01
+
+import (
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/subscription/2021-10-01/subscriptions"
+)
+
+type Client struct {
+	Subscriptions *subscriptions.SubscriptionsClient
+}
+
+func NewClientWithBaseURI(endpoint string, configureAuthFunc func(c *autorest.Client)) Client {
+
+	subscriptionsClient := subscriptions.NewSubscriptionsClientWithBaseURI(endpoint)
+	configureAuthFunc(&subscriptionsClient.Client)
+
+	return Client{
+		Subscriptions: &subscriptionsClient,
+	}
+}
