0c770e68980d28a37daec55dedead219525faa7d
hc-github-team-tf-azure <>
2022-10-24 07:53:18 +0000

    Updating based on befafc31

---
+++ resource-manager/batch/2022-01-01/applicationpackage/README.md
@@ -0,0 +1,111 @@
+
+## `github.com/hashicorp/go-azure-sdk/resource-manager/batch/2022-01-01/applicationpackage` Documentation
+
+The `applicationpackage` SDK allows for interaction with the Azure Resource Manager Service `batch` (API Version `2022-01-01`).
+
+This readme covers example usages, but further information on [using this SDK can be found in the project root](https://github.com/hashicorp/go-azure-sdk/tree/main/docs).
+
+### Import Path
+
+```go
+import "github.com/hashicorp/go-azure-sdk/resource-manager/batch/2022-01-01/applicationpackage"
+```
+
+
+### Client Initialization
+
+```go
+client := applicationpackage.NewApplicationPackageClientWithBaseURI("https://management.azure.com")
+client.Client.Authorizer = authorizer
+```
+
+
+### Example Usage: `ApplicationPackageClient.Activate`
+
+```go
+ctx := context.TODO()
+id := applicationpackage.NewVersionID("12345678-1234-9876-4563-123456789012", "example-resource-group", "accountValue", "applicationValue", "versionValue")
+
+payload := applicationpackage.ActivateApplicationPackageParameters{
+	// ...
+}
+
+
+read, err := client.Activate(ctx, id, payload)
+if err != nil {
+	// handle the error
+}
+if model := read.Model; model != nil {
+	// do something with the model/response object
+}
+```
+
+
+### Example Usage: `ApplicationPackageClient.Create`
+
+```go
+ctx := context.TODO()
+id := applicationpackage.NewVersionID("12345678-1234-9876-4563-123456789012", "example-resource-group", "accountValue", "applicationValue", "versionValue")
+
+payload := applicationpackage.ApplicationPackage{
+	// ...
+}
+
+
+read, err := client.Create(ctx, id, payload)
+if err != nil {
+	// handle the error
+}
+if model := read.Model; model != nil {
+	// do something with the model/response object
+}
+```
+
+
+### Example Usage: `ApplicationPackageClient.Delete`
+
+```go
+ctx := context.TODO()
+id := applicationpackage.NewVersionID("12345678-1234-9876-4563-123456789012", "example-resource-group", "accountValue", "applicationValue", "versionValue")
+
+read, err := client.Delete(ctx, id)
+if err != nil {
+	// handle the error
+}
+if model := read.Model; model != nil {
+	// do something with the model/response object
+}
+```
+
+
+### Example Usage: `ApplicationPackageClient.Get`
+
+```go
+ctx := context.TODO()
+id := applicationpackage.NewVersionID("12345678-1234-9876-4563-123456789012", "example-resource-group", "accountValue", "applicationValue", "versionValue")
+
+read, err := client.Get(ctx, id)
+if err != nil {
+	// handle the error
+}
+if model := read.Model; model != nil {
+	// do something with the model/response object
+}
+```
+
+
+### Example Usage: `ApplicationPackageClient.List`
+
+```go
+ctx := context.TODO()
+id := applicationpackage.NewApplicationID("12345678-1234-9876-4563-123456789012", "example-resource-group", "accountValue", "applicationValue")
+
+// alternatively `client.List(ctx, id, applicationpackage.DefaultListOperationOptions())` can be used to do batched pagination
+items, err := client.ListComplete(ctx, id, applicationpackage.DefaultListOperationOptions())
+if err != nil {
+	// handle the error
+}
+for _, item := range items {
+	// do something
+}
+```
---
+++ resource-manager/batch/2022-01-01/client.go
@@ -0,0 +1,68 @@
+package v2022_01_01
+
+import (
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2022-01-01/application"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2022-01-01/applicationpackage"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2022-01-01/batchaccount"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2022-01-01/batchmanagements"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2022-01-01/certificate"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2022-01-01/location"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2022-01-01/pool"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2022-01-01/privateendpointconnection"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/batch/2022-01-01/privatelinkresource"
+)
+
+type Client struct {
+	Application               *application.ApplicationClient
+	ApplicationPackage        *applicationpackage.ApplicationPackageClient
+	BatchAccount              *batchaccount.BatchAccountClient
+	BatchManagements          *batchmanagements.BatchManagementsClient
+	Certificate               *certificate.CertificateClient
+	Location                  *location.LocationClient
+	Pool                      *pool.PoolClient
+	PrivateEndpointConnection *privateendpointconnection.PrivateEndpointConnectionClient
+	PrivateLinkResource       *privatelinkresource.PrivateLinkResourceClient
+}
+
+func NewClientWithBaseURI(endpoint string, configureAuthFunc func(c *autorest.Client)) Client {
+
+	applicationClient := application.NewApplicationClientWithBaseURI(endpoint)
+	configureAuthFunc(&applicationClient.Client)
+
+	applicationPackageClient := applicationpackage.NewApplicationPackageClientWithBaseURI(endpoint)
+	configureAuthFunc(&applicationPackageClient.Client)
+
+	batchAccountClient := batchaccount.NewBatchAccountClientWithBaseURI(endpoint)
+	configureAuthFunc(&batchAccountClient.Client)
+
+	batchManagementsClient := batchmanagements.NewBatchManagementsClientWithBaseURI(endpoint)
+	configureAuthFunc(&batchManagementsClient.Client)
+
+	certificateClient := certificate.NewCertificateClientWithBaseURI(endpoint)
+	configureAuthFunc(&certificateClient.Client)
+
+	locationClient := location.NewLocationClientWithBaseURI(endpoint)
+	configureAuthFunc(&locationClient.Client)
+
+	poolClient := pool.NewPoolClientWithBaseURI(endpoint)
+	configureAuthFunc(&poolClient.Client)
+
+	privateEndpointConnectionClient := privateendpointconnection.NewPrivateEndpointConnectionClientWithBaseURI(endpoint)
+	configureAuthFunc(&privateEndpointConnectionClient.Client)
+
+	privateLinkResourceClient := privatelinkresource.NewPrivateLinkResourceClientWithBaseURI(endpoint)
+	configureAuthFunc(&privateLinkResourceClient.Client)
+
+	return Client{
+		Application:               &applicationClient,
+		ApplicationPackage:        &applicationPackageClient,
+		BatchAccount:              &batchAccountClient,
+		BatchManagements:          &batchManagementsClient,
+		Certificate:               &certificateClient,
+		Location:                  &locationClient,
+		Pool:                      &poolClient,
+		PrivateEndpointConnection: &privateEndpointConnectionClient,
+		PrivateLinkResource:       &privateLinkResourceClient,
+	}
+}
