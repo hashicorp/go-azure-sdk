08633616c04e664ce8286c2544d458ea4ccbcfca
hc-github-team-tf-azure <>
2022-11-10 16:38:37 +0000

    Updating based on 80c20c4f

---
+++ resource-manager/postgresql/2022-03-08-preview/client.go
@@ -0,0 +1,86 @@
+package v2022_03_08_preview
+
+import (
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2022-03-08-preview/administrators"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2022-03-08-preview/backups"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2022-03-08-preview/checknameavailability"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2022-03-08-preview/configurations"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2022-03-08-preview/databases"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2022-03-08-preview/firewallrules"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2022-03-08-preview/getprivatednszonesuffix"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2022-03-08-preview/locationbasedcapabilities"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2022-03-08-preview/serverrestart"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2022-03-08-preview/servers"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2022-03-08-preview/serverstart"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresql/2022-03-08-preview/serverstop"
+)
+
+type Client struct {
+	Administrators            *administrators.AdministratorsClient
+	Backups                   *backups.BackupsClient
+	CheckNameAvailability     *checknameavailability.CheckNameAvailabilityClient
+	Configurations            *configurations.ConfigurationsClient
+	Databases                 *databases.DatabasesClient
+	FirewallRules             *firewallrules.FirewallRulesClient
+	GetPrivateDnsZoneSuffix   *getprivatednszonesuffix.GetPrivateDnsZoneSuffixClient
+	LocationBasedCapabilities *locationbasedcapabilities.LocationBasedCapabilitiesClient
+	ServerRestart             *serverrestart.ServerRestartClient
+	ServerStart               *serverstart.ServerStartClient
+	ServerStop                *serverstop.ServerStopClient
+	Servers                   *servers.ServersClient
+}
+
+func NewClientWithBaseURI(endpoint string, configureAuthFunc func(c *autorest.Client)) Client {
+
+	administratorsClient := administrators.NewAdministratorsClientWithBaseURI(endpoint)
+	configureAuthFunc(&administratorsClient.Client)
+
+	backupsClient := backups.NewBackupsClientWithBaseURI(endpoint)
+	configureAuthFunc(&backupsClient.Client)
+
+	checkNameAvailabilityClient := checknameavailability.NewCheckNameAvailabilityClientWithBaseURI(endpoint)
+	configureAuthFunc(&checkNameAvailabilityClient.Client)
+
+	configurationsClient := configurations.NewConfigurationsClientWithBaseURI(endpoint)
+	configureAuthFunc(&configurationsClient.Client)
+
+	databasesClient := databases.NewDatabasesClientWithBaseURI(endpoint)
+	configureAuthFunc(&databasesClient.Client)
+
+	firewallRulesClient := firewallrules.NewFirewallRulesClientWithBaseURI(endpoint)
+	configureAuthFunc(&firewallRulesClient.Client)
+
+	getPrivateDnsZoneSuffixClient := getprivatednszonesuffix.NewGetPrivateDnsZoneSuffixClientWithBaseURI(endpoint)
+	configureAuthFunc(&getPrivateDnsZoneSuffixClient.Client)
+
+	locationBasedCapabilitiesClient := locationbasedcapabilities.NewLocationBasedCapabilitiesClientWithBaseURI(endpoint)
+	configureAuthFunc(&locationBasedCapabilitiesClient.Client)
+
+	serverRestartClient := serverrestart.NewServerRestartClientWithBaseURI(endpoint)
+	configureAuthFunc(&serverRestartClient.Client)
+
+	serverStartClient := serverstart.NewServerStartClientWithBaseURI(endpoint)
+	configureAuthFunc(&serverStartClient.Client)
+
+	serverStopClient := serverstop.NewServerStopClientWithBaseURI(endpoint)
+	configureAuthFunc(&serverStopClient.Client)
+
+	serversClient := servers.NewServersClientWithBaseURI(endpoint)
+	configureAuthFunc(&serversClient.Client)
+
+	return Client{
+		Administrators:            &administratorsClient,
+		Backups:                   &backupsClient,
+		CheckNameAvailability:     &checkNameAvailabilityClient,
+		Configurations:            &configurationsClient,
+		Databases:                 &databasesClient,
+		FirewallRules:             &firewallRulesClient,
+		GetPrivateDnsZoneSuffix:   &getPrivateDnsZoneSuffixClient,
+		LocationBasedCapabilities: &locationBasedCapabilitiesClient,
+		ServerRestart:             &serverRestartClient,
+		ServerStart:               &serverStartClient,
+		ServerStop:                &serverStopClient,
+		Servers:                   &serversClient,
+	}
+}
