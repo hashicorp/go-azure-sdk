932ef60f319e1505e4e688dd22b88c761b548021
hc-github-team-tf-azure <>
2022-11-14 16:13:41 +0000

    Updating based on bc2c4b32

--- resource-manager/storage/2021-04-01/managementpolicies/model_dateaftermodification.go
+++
@@ -1,9 +0,0 @@
-package managementpolicies
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type DateAfterModification struct {
-	DaysAfterLastAccessTimeGreaterThan *float64 `json:"daysAfterLastAccessTimeGreaterThan,omitempty"`
-	DaysAfterModificationGreaterThan   *float64 `json:"daysAfterModificationGreaterThan,omitempty"`
-}
