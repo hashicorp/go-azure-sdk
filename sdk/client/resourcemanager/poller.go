6f57bf8936167dfb4b59a3531e2d8a8f38f78ca2
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-12-13 10:28:26 +0100

    resourcemanager: adding a provisioningState poller
    This enables us to dynamically poll on either a LRO or a provisioningState field, if one is found

--- sdk/client/resourcemanager/poller.go
+++ sdk/client/resourcemanager/poller.go
@@ -2,21 +2,49 @@ package resourcemanager
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
 
 	"github.com/hashicorp/go-azure-sdk/sdk/client"
 	"github.com/hashicorp/go-azure-sdk/sdk/client/pollers"
 )
 
 func PollerFromResponse(response *client.Response, client *Client) (poller pollers.Poller, err error) {
-	// TODO: support for checking both the LRO and then the ProvisioningState when this is an ARM Resource
-	// presumably that ^ would be a `unified` poller or something?
-	lroPoller, lroErr := longRunningOperationPollerFromResponse(response, client.Client)
-	if lroErr != nil {
-		return pollers.Poller{}, fmt.Errorf("building long-running-operation poller: %+v", err)
+	if response == nil {
+		return pollers.Poller{}, fmt.Errorf("no HTTP Response was returned")
 	}
 
-	// TODO: polling on the provisioning state based on it being a ResourceID and a PUT/PATCH/POST
-	// response.Request.Method
+	// If this is a LRO we should either have a 201/202 with a Polling URI header
+	isLroStatus := response.StatusCode == http.StatusCreated || response.StatusCode == http.StatusAccepted
+	lroPollingUri := pollingUriForLongRunningOperation(response)
+	if isLroStatus && lroPollingUri != "" {
+		lro, lroErr := longRunningOperationPollerFromResponse(response, client.Client)
+		if lroErr != nil {
+			err = lroErr
+			return pollers.Poller{}, fmt.Errorf("building long-running-operation poller: %+v", lroErr)
+		}
+		return pollers.NewPoller(lro, lro.initialRetryDuration, pollers.DefaultNumberOfDroppedConnectionsToAllow), nil
+	}
 
-	return pollers.NewPoller(lroPoller, lroPoller.initialRetryDuration, pollers.DefaultNumberOfDroppedConnectionsToAllow), nil
+	// or we should be polling on the `provisioningState` of the resource
+	contentType := response.Header.Get("Content-Type")
+	if contentType == "" && response.Request != nil {
+		contentType = response.Request.Header.Get("Content-Type")
+	}
+
+	statusCodesToCheckProvisioningState := response.StatusCode == http.StatusOK || response.StatusCode == http.StatusCreated
+	contentTypeMatchesForProvisioningStateCheck := strings.Contains(strings.ToLower(contentType), "application/json")
+	methodIsApplicable := strings.EqualFold(response.Request.Method, "PATCH") ||
+		strings.EqualFold(response.Request.Method, "POST") ||
+		strings.EqualFold(response.Request.Method, "PUT")
+	if statusCodesToCheckProvisioningState && contentTypeMatchesForProvisioningStateCheck && methodIsApplicable {
+		provisioningState, provisioningStateErr := provisioningStatePollerFromResponse(response, client, DefaultProvisioningStatePollingInterval)
+		if provisioningStateErr != nil {
+			err = provisioningStateErr
+			return pollers.Poller{}, fmt.Errorf("building provisioningState poller: %+v", provisioningStateErr)
+		}
+		return pollers.NewPoller(provisioningState, provisioningState.initialRetryDuration, pollers.DefaultNumberOfDroppedConnectionsToAllow), nil
+	}
+
+	return pollers.Poller{}, fmt.Errorf("no applicable pollers were found for the response")
 }
