912dce5d3a65935d6bc62fc4c5b1e7d9f69d63e1
hc-github-team-tf-azure <>
2023-02-03 13:23:53 +0000

    Updating based on 2ce05cf6

---
+++ resource-manager/databricks/2023-02-01/client.go
@@ -0,0 +1,56 @@
+package v2023_02_01
+
+import (
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2023-02-01/delete"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2023-02-01/outboundnetworkdependenciesendpoints"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2023-02-01/privateendpointconnections"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2023-02-01/privatelinkresources"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2023-02-01/put"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2023-02-01/vnetpeering"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2023-02-01/workspaces"
+)
+
+type Client struct {
+	DELETE                               *delete.DELETEClient
+	OutboundNetworkDependenciesEndpoints *outboundnetworkdependenciesendpoints.OutboundNetworkDependenciesEndpointsClient
+	PUT                                  *put.PUTClient
+	PrivateEndpointConnections           *privateendpointconnections.PrivateEndpointConnectionsClient
+	PrivateLinkResources                 *privatelinkresources.PrivateLinkResourcesClient
+	VNetPeering                          *vnetpeering.VNetPeeringClient
+	Workspaces                           *workspaces.WorkspacesClient
+}
+
+func NewClientWithBaseURI(endpoint string, configureAuthFunc func(c *autorest.Client)) Client {
+
+	dELETEClient := delete.NewDELETEClientWithBaseURI(endpoint)
+	configureAuthFunc(&dELETEClient.Client)
+
+	outboundNetworkDependenciesEndpointsClient := outboundnetworkdependenciesendpoints.NewOutboundNetworkDependenciesEndpointsClientWithBaseURI(endpoint)
+	configureAuthFunc(&outboundNetworkDependenciesEndpointsClient.Client)
+
+	pUTClient := put.NewPUTClientWithBaseURI(endpoint)
+	configureAuthFunc(&pUTClient.Client)
+
+	privateEndpointConnectionsClient := privateendpointconnections.NewPrivateEndpointConnectionsClientWithBaseURI(endpoint)
+	configureAuthFunc(&privateEndpointConnectionsClient.Client)
+
+	privateLinkResourcesClient := privatelinkresources.NewPrivateLinkResourcesClientWithBaseURI(endpoint)
+	configureAuthFunc(&privateLinkResourcesClient.Client)
+
+	vNetPeeringClient := vnetpeering.NewVNetPeeringClientWithBaseURI(endpoint)
+	configureAuthFunc(&vNetPeeringClient.Client)
+
+	workspacesClient := workspaces.NewWorkspacesClientWithBaseURI(endpoint)
+	configureAuthFunc(&workspacesClient.Client)
+
+	return Client{
+		DELETE:                               &dELETEClient,
+		OutboundNetworkDependenciesEndpoints: &outboundNetworkDependenciesEndpointsClient,
+		PUT:                                  &pUTClient,
+		PrivateEndpointConnections:           &privateEndpointConnectionsClient,
+		PrivateLinkResources:                 &privateLinkResourcesClient,
+		VNetPeering:                          &vNetPeeringClient,
+		Workspaces:                           &workspacesClient,
+	}
+}
---
+++ resource-manager/databricks/2023-02-01/outboundnetworkdependenciesendpoints/README.md
@@ -0,0 +1,36 @@
+
+## `github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2023-02-01/outboundnetworkdependenciesendpoints` Documentation
+
+The `outboundnetworkdependenciesendpoints` SDK allows for interaction with the Azure Resource Manager Service `databricks` (API Version `2023-02-01`).
+
+This readme covers example usages, but further information on [using this SDK can be found in the project root](https://github.com/hashicorp/go-azure-sdk/tree/main/docs).
+
+### Import Path
+
+```go
+import "github.com/hashicorp/go-azure-sdk/resource-manager/databricks/2023-02-01/outboundnetworkdependenciesendpoints"
+```
+
+
+### Client Initialization
+
+```go
+client := outboundnetworkdependenciesendpoints.NewOutboundNetworkDependenciesEndpointsClientWithBaseURI("https://management.azure.com")
+client.Client.Authorizer = authorizer
+```
+
+
+### Example Usage: `OutboundNetworkDependenciesEndpointsClient.List`
+
+```go
+ctx := context.TODO()
+id := outboundnetworkdependenciesendpoints.NewWorkspaceID("12345678-1234-9876-4563-123456789012", "example-resource-group", "workspaceValue")
+
+read, err := client.List(ctx, id)
+if err != nil {
+	// handle the error
+}
+if model := read.Model; model != nil {
+	// do something with the model/response object
+}
+```
