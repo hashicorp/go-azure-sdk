451a9068f28d0c9f4565fd9ea80ceded9a68cdb6
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-02-22 10:29:34 +0100

    tooling: changing the open pr automation to use a shell script and forcing an exit 0

---
+++ scripts/open-pull-request.sh
@@ -0,0 +1,14 @@
+function main {
+  local prTitle="$1"
+  local prBody="$2"
+  local branch="$3"
+  local label="$4"
+
+  # this runs everytime the PR gets pushed too, whilst you can only create a PR a single time
+  # so we should be smarter, but just trying to trigger this should be sufficient for now
+  result=$(gh pr create --title "$prTitle" --body "$prBody" -B "$branch" -l "$label")
+  echo "$result"
+  exit 0
+}
+
+main "$1" "$2" "$3" "$4"
