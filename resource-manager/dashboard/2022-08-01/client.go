5d07d1fcc972f1aea04fecca902f0a8c787cc5e2
hc-github-team-tf-azure <>
2023-03-06 19:15:05 +0000

    Updating based on 1b5e3587

--- resource-manager/dashboard/2022-08-01/client.go
+++ resource-manager/dashboard/2022-08-01/client.go
@@ -1,10 +1,13 @@
 package v2022_08_01
 
 import (
-	"github.com/Azure/go-autorest/autorest"
+	"fmt"
+
 	"github.com/hashicorp/go-azure-sdk/resource-manager/dashboard/2022-08-01/grafanaresource"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/dashboard/2022-08-01/privateendpointconnection"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/dashboard/2022-08-01/privatelinkresource"
+	"github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager"
+	"github.com/hashicorp/go-azure-sdk/sdk/environments"
 )
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
@@ -16,20 +19,28 @@ type Client struct {
 	PrivateLinkResource       *privatelinkresource.PrivateLinkResourceClient
 }
 
-func NewClientWithBaseURI(endpoint string, configureAuthFunc func(c *autorest.Client)) Client {
-
-	grafanaResourceClient := grafanaresource.NewGrafanaResourceClientWithBaseURI(endpoint)
-	configureAuthFunc(&grafanaResourceClient.Client)
-
-	privateEndpointConnectionClient := privateendpointconnection.NewPrivateEndpointConnectionClientWithBaseURI(endpoint)
-	configureAuthFunc(&privateEndpointConnectionClient.Client)
-
-	privateLinkResourceClient := privatelinkresource.NewPrivateLinkResourceClientWithBaseURI(endpoint)
-	configureAuthFunc(&privateLinkResourceClient.Client)
-
-	return Client{
-		GrafanaResource:           &grafanaResourceClient,
-		PrivateEndpointConnection: &privateEndpointConnectionClient,
-		PrivateLinkResource:       &privateLinkResourceClient,
+func NewClientWithBaseURI(api environments.Api, configureFunc func(c *resourcemanager.Client)) (*Client, error) {
+	grafanaResourceClient, err := grafanaresource.NewGrafanaResourceClientWithBaseURI(api)
+	if err != nil {
+		return nil, fmt.Errorf("building GrafanaResource client: %+v", err)
 	}
+	configureFunc(grafanaResourceClient.Client)
+
+	privateEndpointConnectionClient, err := privateendpointconnection.NewPrivateEndpointConnectionClientWithBaseURI(api)
+	if err != nil {
+		return nil, fmt.Errorf("building PrivateEndpointConnection client: %+v", err)
+	}
+	configureFunc(privateEndpointConnectionClient.Client)
+
+	privateLinkResourceClient, err := privatelinkresource.NewPrivateLinkResourceClientWithBaseURI(api)
+	if err != nil {
+		return nil, fmt.Errorf("building PrivateLinkResource client: %+v", err)
+	}
+	configureFunc(privateLinkResourceClient.Client)
+
+	return &Client{
+		GrafanaResource:           grafanaResourceClient,
+		PrivateEndpointConnection: privateEndpointConnectionClient,
+		PrivateLinkResource:       privateLinkResourceClient,
+	}, nil
 }
