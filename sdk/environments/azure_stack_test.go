7259b58883b79011dfdbe3ef21ba275f19bd2162
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-03-07 18:16:03 +0100

    sdk/environments: porting the `IsAzureStack` method to `go-azure-sdk`

---
+++ sdk/environments/azure_stack_test.go
@@ -0,0 +1,62 @@
+package environments
+
+import (
+	"log"
+	"testing"
+)
+
+func TestIsAzureStack_BuiltInEnvironmentsShouldNotBeMarkedAsStack(t *testing.T) {
+	testData := []*Environment{
+		AzurePublic(),
+		AzurePublicCanary(),
+		AzureChina(),
+		AzureUSGovernment(),
+		AzureUSGovernmentL5(),
+	}
+	for _, v := range testData {
+		log.Printf("Environment: %q", v.Name)
+		if v.IsAzureStack() {
+			t.Fatalf("expected IsAzureStack to be false but was true for Environment %q", v.Name)
+		}
+	}
+}
+
+func TestIsAzureStack(t *testing.T) {
+	testData := []*Environment{
+		{
+			Name: "AzureStackCloud",
+		},
+		{
+			Name: "Identity Provider Empty",
+			Authorization: &Authorization{
+				IdentityProvider: "",
+			},
+		},
+		{
+			Name: "Identity Provider Custom",
+			Authorization: &Authorization{
+				IdentityProvider: "Custom",
+			},
+		},
+		{
+			Name: "Tenant Empty",
+			Authorization: &Authorization{
+				IdentityProvider: "AAD",
+				Tenant:           "",
+			},
+		},
+		{
+			Name: "Tenant Custom",
+			Authorization: &Authorization{
+				IdentityProvider: "AAD",
+				Tenant:           "Custom",
+			},
+		},
+	}
+	for _, v := range testData {
+		log.Printf("Environment %q", v.Name)
+		if !v.IsAzureStack() {
+			t.Fatalf("expected IsAzureStack to be true but was false for Environment %q", v.Name)
+		}
+	}
+}
