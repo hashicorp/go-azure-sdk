5a16e8fe21e71ead3ae6056d63461fb7a4d6ee04
hc-github-team-tf-azure <>
2023-03-10 15:45:52 +0000

    Updating based on ef836f05

--- resource-manager/datafactory/2018-06-01/dataflowdebugsession/model_dataset.go
+++ resource-manager/datafactory/2018-06-01/dataflowdebugsession/model_dataset.go
@@ -1,5 +1,10 @@
 package dataflowdebugsession
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
@@ -12,4 +17,36 @@ type Dataset struct {
 	Schema            *interface{}                       `json:"schema,omitempty"`
 	Structure         *interface{}                       `json:"structure,omitempty"`
 	Type              string                             `json:"type"`
+}
+
+var _ json.Unmarshaler = &Dataset{}
+
+func (s *Dataset) UnmarshalJSON(bytes []byte) error {
+	type alias Dataset
+	var decoded alias
+	if err := json.Unmarshal(bytes, &decoded); err != nil {
+		return fmt.Errorf("unmarshaling into Dataset: %+v", err)
+	}
+
+	s.Annotations = decoded.Annotations
+	s.Description = decoded.Description
+	s.Folder = decoded.Folder
+	s.Parameters = decoded.Parameters
+	s.Schema = decoded.Schema
+	s.Structure = decoded.Structure
+	s.Type = decoded.Type
+
+	var temp map[string]json.RawMessage
+	if err := json.Unmarshal(bytes, &temp); err != nil {
+		return fmt.Errorf("unmarshaling Dataset into map[string]json.RawMessage: %+v", err)
+	}
+
+	if v, ok := temp["linkedServiceName"]; ok {
+		impl, err := unmarshalLinkedServiceReferenceImplementation(v)
+		if err != nil {
+			return fmt.Errorf("unmarshaling field 'LinkedServiceName' for 'Dataset': %+v", err)
+		}
+		s.LinkedServiceName = impl
+	}
+	return nil
 }
--- resource-manager/datafactory/2018-06-01/dataflowdebugsession/model_linkedservice.go
+++ resource-manager/datafactory/2018-06-01/dataflowdebugsession/model_linkedservice.go
@@ -1,5 +1,10 @@
 package dataflowdebugsession
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
@@ -9,4 +14,33 @@ type LinkedService struct {
 	Description *string                            `json:"description,omitempty"`
 	Parameters  *map[string]ParameterSpecification `json:"parameters,omitempty"`
 	Type        string                             `json:"type"`
+}
+
+var _ json.Unmarshaler = &LinkedService{}
+
+func (s *LinkedService) UnmarshalJSON(bytes []byte) error {
+	type alias LinkedService
+	var decoded alias
+	if err := json.Unmarshal(bytes, &decoded); err != nil {
+		return fmt.Errorf("unmarshaling into LinkedService: %+v", err)
+	}
+
+	s.Annotations = decoded.Annotations
+	s.Description = decoded.Description
+	s.Parameters = decoded.Parameters
+	s.Type = decoded.Type
+
+	var temp map[string]json.RawMessage
+	if err := json.Unmarshal(bytes, &temp); err != nil {
+		return fmt.Errorf("unmarshaling LinkedService into map[string]json.RawMessage: %+v", err)
+	}
+
+	if v, ok := temp["connectVia"]; ok {
+		impl, err := unmarshalIntegrationRuntimeReferenceImplementation(v)
+		if err != nil {
+			return fmt.Errorf("unmarshaling field 'ConnectVia' for 'LinkedService': %+v", err)
+		}
+		s.ConnectVia = impl
+	}
+	return nil
 }
