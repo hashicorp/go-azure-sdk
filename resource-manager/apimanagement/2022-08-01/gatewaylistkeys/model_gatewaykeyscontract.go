343a6dc98d5ec832f1369f88f0e87c825ca5c530
hc-github-team-tf-azure <>
2023-03-15 10:15:29 +0000

    Updating based on 01620208

--- resource-manager/apimanagement/2022-08-01/gatewaylistkeys/model_gatewaykeyscontract.go
+++
@@ -1,9 +0,0 @@
-package gatewaylistkeys
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type GatewayKeysContract struct {
-	Primary   *string `json:"primary,omitempty"`
-	Secondary *string `json:"secondary,omitempty"`
-}
