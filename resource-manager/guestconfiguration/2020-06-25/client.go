4b30f2ec76152226565171c5272638441ba0f37f
hc-github-team-tf-azure <>
2023-04-24 19:39:01 +0000

    Updating based on 4b80bdaa

---
+++ resource-manager/guestconfiguration/2020-06-25/client.go
@@ -0,0 +1,73 @@
+package v2020_06_25
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/go-azure-sdk/resource-manager/guestconfiguration/2020-06-25/guestconfigurationassignmenthcrpreports"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/guestconfiguration/2020-06-25/guestconfigurationassignmentreports"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/guestconfiguration/2020-06-25/guestconfigurationassignments"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/guestconfiguration/2020-06-25/guestconfigurationconnectedvmwarevsphereassignments"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/guestconfiguration/2020-06-25/guestconfigurationconnectedvmwarevsphereassignmentsreports"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/guestconfiguration/2020-06-25/guestconfigurationhcrpassignments"
+	"github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager"
+	"github.com/hashicorp/go-azure-sdk/sdk/environments"
+)
+
+type Client struct {
+	GuestConfigurationAssignmentHCRPReports                    *guestconfigurationassignmenthcrpreports.GuestConfigurationAssignmentHCRPReportsClient
+	GuestConfigurationAssignmentReports                        *guestconfigurationassignmentreports.GuestConfigurationAssignmentReportsClient
+	GuestConfigurationAssignments                              *guestconfigurationassignments.GuestConfigurationAssignmentsClient
+	GuestConfigurationConnectedVMwarevSphereAssignments        *guestconfigurationconnectedvmwarevsphereassignments.GuestConfigurationConnectedVMwarevSphereAssignmentsClient
+	GuestConfigurationConnectedVMwarevSphereAssignmentsReports *guestconfigurationconnectedvmwarevsphereassignmentsreports.GuestConfigurationConnectedVMwarevSphereAssignmentsReportsClient
+	GuestConfigurationHCRPAssignments                          *guestconfigurationhcrpassignments.GuestConfigurationHCRPAssignmentsClient
+}
+
+func NewClientWithBaseURI(api environments.Api, configureFunc func(c *resourcemanager.Client)) (*Client, error) {
+	guestConfigurationAssignmentHCRPReportsClient, err := guestconfigurationassignmenthcrpreports.NewGuestConfigurationAssignmentHCRPReportsClientWithBaseURI(api)
+	if err != nil {
+		return nil, fmt.Errorf("building GuestConfigurationAssignmentHCRPReports client: %+v", err)
+	}
+	configureFunc(guestConfigurationAssignmentHCRPReportsClient.Client)
+
+	guestConfigurationAssignmentReportsClient, err := guestconfigurationassignmentreports.NewGuestConfigurationAssignmentReportsClientWithBaseURI(api)
+	if err != nil {
+		return nil, fmt.Errorf("building GuestConfigurationAssignmentReports client: %+v", err)
+	}
+	configureFunc(guestConfigurationAssignmentReportsClient.Client)
+
+	guestConfigurationAssignmentsClient, err := guestconfigurationassignments.NewGuestConfigurationAssignmentsClientWithBaseURI(api)
+	if err != nil {
+		return nil, fmt.Errorf("building GuestConfigurationAssignments client: %+v", err)
+	}
+	configureFunc(guestConfigurationAssignmentsClient.Client)
+
+	guestConfigurationConnectedVMwarevSphereAssignmentsClient, err := guestconfigurationconnectedvmwarevsphereassignments.NewGuestConfigurationConnectedVMwarevSphereAssignmentsClientWithBaseURI(api)
+	if err != nil {
+		return nil, fmt.Errorf("building GuestConfigurationConnectedVMwarevSphereAssignments client: %+v", err)
+	}
+	configureFunc(guestConfigurationConnectedVMwarevSphereAssignmentsClient.Client)
+
+	guestConfigurationConnectedVMwarevSphereAssignmentsReportsClient, err := guestconfigurationconnectedvmwarevsphereassignmentsreports.NewGuestConfigurationConnectedVMwarevSphereAssignmentsReportsClientWithBaseURI(api)
+	if err != nil {
+		return nil, fmt.Errorf("building GuestConfigurationConnectedVMwarevSphereAssignmentsReports client: %+v", err)
+	}
+	configureFunc(guestConfigurationConnectedVMwarevSphereAssignmentsReportsClient.Client)
+
+	guestConfigurationHCRPAssignmentsClient, err := guestconfigurationhcrpassignments.NewGuestConfigurationHCRPAssignmentsClientWithBaseURI(api)
+	if err != nil {
+		return nil, fmt.Errorf("building GuestConfigurationHCRPAssignments client: %+v", err)
+	}
+	configureFunc(guestConfigurationHCRPAssignmentsClient.Client)
+
+	return &Client{
+		GuestConfigurationAssignmentHCRPReports:                    guestConfigurationAssignmentHCRPReportsClient,
+		GuestConfigurationAssignmentReports:                        guestConfigurationAssignmentReportsClient,
+		GuestConfigurationAssignments:                              guestConfigurationAssignmentsClient,
+		GuestConfigurationConnectedVMwarevSphereAssignments:        guestConfigurationConnectedVMwarevSphereAssignmentsClient,
+		GuestConfigurationConnectedVMwarevSphereAssignmentsReports: guestConfigurationConnectedVMwarevSphereAssignmentsReportsClient,
+		GuestConfigurationHCRPAssignments:                          guestConfigurationHCRPAssignmentsClient,
+	}, nil
+}
--- resource-manager/recoveryservicesbackup/2023-02-01/privateendpointconnection/constants.go
+++ resource-manager/recoveryservicesbackup/2023-02-01/privateendpointconnection/constants.go
@@ -72,3 +72,34 @@ func parseProvisioningState(input string) (*ProvisioningState, error) {
 	out := ProvisioningState(input)
 	return &out, nil
 }
+
+type VaultSubResourceType string
+
+const (
+	VaultSubResourceTypeAzureBackup          VaultSubResourceType = "AzureBackup"
+	VaultSubResourceTypeAzureBackupSecondary VaultSubResourceType = "AzureBackup_secondary"
+	VaultSubResourceTypeAzureSiteRecovery    VaultSubResourceType = "AzureSiteRecovery"
+)
+
+func PossibleValuesForVaultSubResourceType() []string {
+	return []string{
+		string(VaultSubResourceTypeAzureBackup),
+		string(VaultSubResourceTypeAzureBackupSecondary),
+		string(VaultSubResourceTypeAzureSiteRecovery),
+	}
+}
+
+func parseVaultSubResourceType(input string) (*VaultSubResourceType, error) {
+	vals := map[string]VaultSubResourceType{
+		"azurebackup":           VaultSubResourceTypeAzureBackup,
+		"azurebackup_secondary": VaultSubResourceTypeAzureBackupSecondary,
+		"azuresiterecovery":     VaultSubResourceTypeAzureSiteRecovery,
+	}
+	if v, ok := vals[strings.ToLower(input)]; ok {
+		return &v, nil
+	}
+
+	// otherwise presume it's an undefined value and best-effort it
+	out := VaultSubResourceType(input)
+	return &out, nil
+}
