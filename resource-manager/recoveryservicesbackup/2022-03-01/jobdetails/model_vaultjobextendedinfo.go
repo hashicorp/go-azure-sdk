1e7b88609ac792d02150085b12dd90bafb25625a
hc-github-team-tf-azure <>
2023-05-05 07:33:33 +0000

    Updating based on 5cd68155

--- resource-manager/recoveryservicesbackup/2022-03-01/jobdetails/model_vaultjobextendedinfo.go
+++
@@ -1,8 +0,0 @@
-package jobdetails
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type VaultJobExtendedInfo struct {
-	PropertyBag *map[string]string `json:"propertyBag,omitempty"`
-}
--- resource-manager/recoveryservicesbackup/2022-03-01/recoverypointsrecommendedformove/model_listrecoverypointsrecommendedformoverequest.go
+++
@@ -1,9 +0,0 @@
-package recoverypointsrecommendedformove
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type ListRecoveryPointsRecommendedForMoveRequest struct {
-	ExcludedRPList *[]string `json:"excludedRPList,omitempty"`
-	ObjectType     *string   `json:"objectType,omitempty"`
-}
