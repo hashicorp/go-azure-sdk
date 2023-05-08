89057d93d25ddd2595098ca1c032d4be104d0af9
hc-github-team-tf-azure <>
2023-05-08 13:46:22 +0000

    Updating based on 54a80d14

---
+++ resource-manager/maintenance/2023-04-01/applyupdates/method_createorupdate_autorest.go
@@ -0,0 +1,69 @@
+package applyupdates
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/azure"
+)
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type CreateOrUpdateOperationResponse struct {
+	HttpResponse *http.Response
+	Model        *ApplyUpdate
+}
+
+// CreateOrUpdate ...
+func (c ApplyUpdatesClient) CreateOrUpdate(ctx context.Context, id ProviderId) (result CreateOrUpdateOperationResponse, err error) {
+	req, err := c.preparerForCreateOrUpdate(ctx, id)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "applyupdates.ApplyUpdatesClient", "CreateOrUpdate", nil, "Failure preparing request")
+		return
+	}
+
+	result.HttpResponse, err = c.Client.Send(req, azure.DoRetryWithRegistration(c.Client))
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "applyupdates.ApplyUpdatesClient", "CreateOrUpdate", result.HttpResponse, "Failure sending request")
+		return
+	}
+
+	result, err = c.responderForCreateOrUpdate(result.HttpResponse)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "applyupdates.ApplyUpdatesClient", "CreateOrUpdate", result.HttpResponse, "Failure responding to request")
+		return
+	}
+
+	return
+}
+
+// preparerForCreateOrUpdate prepares the CreateOrUpdate request.
+func (c ApplyUpdatesClient) preparerForCreateOrUpdate(ctx context.Context, id ProviderId) (*http.Request, error) {
+	queryParameters := map[string]interface{}{
+		"api-version": defaultApiVersion,
+	}
+
+	preparer := autorest.CreatePreparer(
+		autorest.AsContentType("application/json; charset=utf-8"),
+		autorest.AsPut(),
+		autorest.WithBaseURL(c.baseUri),
+		autorest.WithPath(fmt.Sprintf("%s/providers/Microsoft.Maintenance/applyUpdates/default", id.ID())),
+		autorest.WithQueryParameters(queryParameters))
+	return preparer.Prepare((&http.Request{}).WithContext(ctx))
+}
+
+// responderForCreateOrUpdate handles the response to the CreateOrUpdate request. The method always
+// closes the http.Response Body.
+func (c ApplyUpdatesClient) responderForCreateOrUpdate(resp *http.Response) (result CreateOrUpdateOperationResponse, err error) {
+	err = autorest.Respond(
+		resp,
+		azure.WithErrorUnlessStatusCode(http.StatusCreated, http.StatusOK),
+		autorest.ByUnmarshallingJSON(&result.Model),
+		autorest.ByClosing())
+	result.HttpResponse = resp
+
+	return
+}
---
+++ resource-manager/maintenance/2023-04-01/configurationassignments/method_forsubscriptionsdelete_autorest.go
@@ -0,0 +1,68 @@
+package configurationassignments
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/azure"
+)
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type ForSubscriptionsDeleteOperationResponse struct {
+	HttpResponse *http.Response
+	Model        *ConfigurationAssignment
+}
+
+// ForSubscriptionsDelete ...
+func (c ConfigurationAssignmentsClient) ForSubscriptionsDelete(ctx context.Context, id ConfigurationAssignmentId) (result ForSubscriptionsDeleteOperationResponse, err error) {
+	req, err := c.preparerForForSubscriptionsDelete(ctx, id)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "configurationassignments.ConfigurationAssignmentsClient", "ForSubscriptionsDelete", nil, "Failure preparing request")
+		return
+	}
+
+	result.HttpResponse, err = c.Client.Send(req, azure.DoRetryWithRegistration(c.Client))
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "configurationassignments.ConfigurationAssignmentsClient", "ForSubscriptionsDelete", result.HttpResponse, "Failure sending request")
+		return
+	}
+
+	result, err = c.responderForForSubscriptionsDelete(result.HttpResponse)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "configurationassignments.ConfigurationAssignmentsClient", "ForSubscriptionsDelete", result.HttpResponse, "Failure responding to request")
+		return
+	}
+
+	return
+}
+
+// preparerForForSubscriptionsDelete prepares the ForSubscriptionsDelete request.
+func (c ConfigurationAssignmentsClient) preparerForForSubscriptionsDelete(ctx context.Context, id ConfigurationAssignmentId) (*http.Request, error) {
+	queryParameters := map[string]interface{}{
+		"api-version": defaultApiVersion,
+	}
+
+	preparer := autorest.CreatePreparer(
+		autorest.AsContentType("application/json; charset=utf-8"),
+		autorest.AsDelete(),
+		autorest.WithBaseURL(c.baseUri),
+		autorest.WithPath(id.ID()),
+		autorest.WithQueryParameters(queryParameters))
+	return preparer.Prepare((&http.Request{}).WithContext(ctx))
+}
+
+// responderForForSubscriptionsDelete handles the response to the ForSubscriptionsDelete request. The method always
+// closes the http.Response Body.
+func (c ConfigurationAssignmentsClient) responderForForSubscriptionsDelete(resp *http.Response) (result ForSubscriptionsDeleteOperationResponse, err error) {
+	err = autorest.Respond(
+		resp,
+		azure.WithErrorUnlessStatusCode(http.StatusAccepted, http.StatusNoContent, http.StatusOK),
+		autorest.ByUnmarshallingJSON(&result.Model),
+		autorest.ByClosing())
+	result.HttpResponse = resp
+
+	return
+}
