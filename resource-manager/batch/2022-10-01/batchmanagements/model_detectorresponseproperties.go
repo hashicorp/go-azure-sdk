9cfa21e00c94d672f436ce2af778b2f9767d270a
hc-github-team-tf-azure <>
2023-07-12 07:37:28 +0000

    Updating based on f545ad0d6

--- resource-manager/batch/2022-10-01/batchmanagements/model_detectorresponseproperties.go
+++
@@ -1,8 +0,0 @@
-package batchmanagements
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type DetectorResponseProperties struct {
-	Value *string `json:"value,omitempty"`
-}
