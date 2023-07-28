ef1ac4f7874fc7d6042057cce626f6adec68db78
hc-github-team-tf-azure <>
2023-07-28 05:54:14 +0000

    Updating based on 3b4e3d772

---
+++ resource-manager/containerapps/2022-11-01-preview/billingmeters/version.go
@@ -0,0 +1,12 @@
+package billingmeters
+
+import "fmt"
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+const defaultApiVersion = "2022-11-01-preview"
+
+func userAgent() string {
+	return fmt.Sprintf("hashicorp/go-azure-sdk/billingmeters/%s", defaultApiVersion)
+}
