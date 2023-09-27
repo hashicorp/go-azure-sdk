60af3f3e689c10917bccc4460267e8d7743a9879
Tom Bamford <tom@bamford.io>
2023-09-27 01:03:49 +0100

    auth: fix a bug with auxiliary token caching

--- sdk/auth/cached_authorizer.go
+++ sdk/auth/cached_authorizer.go
@@ -55,7 +55,7 @@ func (c *CachedAuthorizer) AuxiliaryTokens(ctx context.Context, req *http.Reques
 	}
 	c.mutex.RUnlock()
 
-	if !dueForRenewal {
+	if dueForRenewal {
 		c.mutex.Lock()
 		defer c.mutex.Unlock()
 		var err error
