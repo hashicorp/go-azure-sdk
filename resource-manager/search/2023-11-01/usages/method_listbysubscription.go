c52f8c85ba6f95b9d4263c7be15cf1c434f29b00
hc-github-team-tf-azure <>
2023-10-11 08:04:10 +0000

    Updating based on b2312aedf

---
+++ resource-manager/search/2023-11-01/usages/method_listbysubscription.go
@@ -0,0 +1,117 @@
+package usages
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/hashicorp/go-azure-sdk/sdk/client"
+	"github.com/hashicorp/go-azure-sdk/sdk/odata"
+)
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type ListBySubscriptionOperationResponse struct {
+	HttpResponse *http.Response
+	OData        *odata.OData
+	Model        *[]QuotaUsageResult
+}
+
+type ListBySubscriptionCompleteResult struct {
+	Items []QuotaUsageResult
+}
+
+type ListBySubscriptionOperationOptions struct {
+	XMsClientRequestId *string
+}
+
+func DefaultListBySubscriptionOperationOptions() ListBySubscriptionOperationOptions {
+	return ListBySubscriptionOperationOptions{}
+}
+
+func (o ListBySubscriptionOperationOptions) ToHeaders() *client.Headers {
+	out := client.Headers{}
+	if o.XMsClientRequestId != nil {
+		out.Append("x-ms-client-request-id", fmt.Sprintf("%v", *o.XMsClientRequestId))
+	}
+	return &out
+}
+
+func (o ListBySubscriptionOperationOptions) ToOData() *odata.Query {
+	out := odata.Query{}
+	return &out
+}
+
+func (o ListBySubscriptionOperationOptions) ToQuery() *client.QueryParams {
+	out := client.QueryParams{}
+
+	return &out
+}
+
+// ListBySubscription ...
+func (c UsagesClient) ListBySubscription(ctx context.Context, id LocationId, options ListBySubscriptionOperationOptions) (result ListBySubscriptionOperationResponse, err error) {
+	opts := client.RequestOptions{
+		ContentType: "application/json; charset=utf-8",
+		ExpectedStatusCodes: []int{
+			http.StatusOK,
+		},
+		HttpMethod:    http.MethodGet,
+		Path:          fmt.Sprintf("%s/usages", id.ID()),
+		OptionsObject: options,
+	}
+
+	req, err := c.Client.NewRequest(ctx, opts)
+	if err != nil {
+		return
+	}
+
+	var resp *client.Response
+	resp, err = req.ExecutePaged(ctx)
+	if resp != nil {
+		result.OData = resp.OData
+		result.HttpResponse = resp.Response
+	}
+	if err != nil {
+		return
+	}
+
+	var values struct {
+		Values *[]QuotaUsageResult `json:"value"`
+	}
+	if err = resp.Unmarshal(&values); err != nil {
+		return
+	}
+
+	result.Model = values.Values
+
+	return
+}
+
+// ListBySubscriptionComplete retrieves all the results into a single object
+func (c UsagesClient) ListBySubscriptionComplete(ctx context.Context, id LocationId, options ListBySubscriptionOperationOptions) (ListBySubscriptionCompleteResult, error) {
+	return c.ListBySubscriptionCompleteMatchingPredicate(ctx, id, options, QuotaUsageResultOperationPredicate{})
+}
+
+// ListBySubscriptionCompleteMatchingPredicate retrieves all the results and then applies the predicate
+func (c UsagesClient) ListBySubscriptionCompleteMatchingPredicate(ctx context.Context, id LocationId, options ListBySubscriptionOperationOptions, predicate QuotaUsageResultOperationPredicate) (result ListBySubscriptionCompleteResult, err error) {
+	items := make([]QuotaUsageResult, 0)
+
+	resp, err := c.ListBySubscription(ctx, id, options)
+	if err != nil {
+		err = fmt.Errorf("loading results: %+v", err)
+		return
+	}
+	if resp.Model != nil {
+		for _, v := range *resp.Model {
+			if predicate.Matches(v) {
+				items = append(items, v)
+			}
+		}
+	}
+
+	result = ListBySubscriptionCompleteResult{
+		Items: items,
+	}
+	return
+}
