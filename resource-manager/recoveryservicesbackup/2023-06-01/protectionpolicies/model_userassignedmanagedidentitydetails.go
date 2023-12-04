e56e6d1b3e706b0884bb4c37e3fa3ce8da3abd32
hc-github-team-tf-azure <>
2023-12-04 19:30:03 +0000

    Updating based on bd7a0201c

--- resource-manager/recoveryservicesbackup/2023-06-01/protectionpolicies/model_userassignedmanagedidentitydetails.go
+++ resource-manager/recoveryservicesbackup/2023-06-01/protectionpolicies/model_userassignedmanagedidentitydetails.go
@@ -4,7 +4,7 @@ package protectionpolicies
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
 type UserAssignedManagedIdentityDetails struct {
-	IdentityArmId                  *string               `json:"identityArmId,omitempty"`
-	IdentityName                   *string               `json:"identityName,omitempty"`
-	UserAssignedIdentityProperties *UserAssignedIdentity `json:"userAssignedIdentityProperties,omitempty"`
+	IdentityArmId                  *string                         `json:"identityArmId,omitempty"`
+	IdentityName                   *string                         `json:"identityName,omitempty"`
+	UserAssignedIdentityProperties *UserAssignedIdentityProperties `json:"userAssignedIdentityProperties,omitempty"`
 }
