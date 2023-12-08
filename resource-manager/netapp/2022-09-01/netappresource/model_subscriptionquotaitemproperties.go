9e47dc7e8d70c7640fbfacc60f985bac630d7942
hc-github-team-tf-azure <>
2023-12-08 12:46:23 +0000

    Updating based on 91847d75a

--- resource-manager/netapp/2022-09-01/netappresource/model_subscriptionquotaitemproperties.go
+++
@@ -1,9 +0,0 @@
-package netappresource
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type SubscriptionQuotaItemProperties struct {
-	Current *int64 `json:"current,omitempty"`
-	Default *int64 `json:"default,omitempty"`
-}
