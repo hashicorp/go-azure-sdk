e9a835ad164b20ff2fe0bc378de320e96c26bce6
hc-github-team-tf-azure <>
2023-12-11 13:32:34 +0000

    Updating based on c654665af

---
+++ resource-manager/insights/2023-10-01/metricdefinitions/client.go
@@ -0,0 +1,18 @@
+package metricdefinitions
+
+import "github.com/Azure/go-autorest/autorest"
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type MetricDefinitionsClient struct {
+	Client  autorest.Client
+	baseUri string
+}
+
+func NewMetricDefinitionsClientWithBaseURI(endpoint string) MetricDefinitionsClient {
+	return MetricDefinitionsClient{
+		Client:  autorest.NewClientWithUserAgent(userAgent()),
+		baseUri: endpoint,
+	}
+}
