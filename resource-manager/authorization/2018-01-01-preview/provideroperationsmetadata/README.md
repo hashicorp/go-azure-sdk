07760ee2006fa4fd1195bd732180897b4510c7d8
hc-github-team-tf-azure <>
2023-12-15 11:22:55 +0000

    Updating based on 85ef9f4a1

---
+++ resource-manager/authorization/2018-01-01-preview/provideroperationsmetadata/README.md
@@ -0,0 +1,53 @@
+
+## `github.com/hashicorp/go-azure-sdk/resource-manager/authorization/2018-01-01-preview/provideroperationsmetadata` Documentation
+
+The `provideroperationsmetadata` SDK allows for interaction with the Azure Resource Manager Service `authorization` (API Version `2018-01-01-preview`).
+
+This readme covers example usages, but further information on [using this SDK can be found in the project root](https://github.com/hashicorp/go-azure-sdk/tree/main/docs).
+
+### Import Path
+
+```go
+import "github.com/hashicorp/go-azure-sdk/resource-manager/authorization/2018-01-01-preview/provideroperationsmetadata"
+```
+
+
+### Client Initialization
+
+```go
+client := provideroperationsmetadata.NewProviderOperationsMetadataClientWithBaseURI("https://management.azure.com")
+client.Client.Authorizer = authorizer
+```
+
+
+### Example Usage: `ProviderOperationsMetadataClient.Get`
+
+```go
+ctx := context.TODO()
+id := provideroperationsmetadata.NewProviderOperationID("providerOperationValue")
+
+read, err := client.Get(ctx, id, provideroperationsmetadata.DefaultGetOperationOptions())
+if err != nil {
+	// handle the error
+}
+if model := read.Model; model != nil {
+	// do something with the model/response object
+}
+```
+
+
+### Example Usage: `ProviderOperationsMetadataClient.List`
+
+```go
+ctx := context.TODO()
+
+
+// alternatively `client.List(ctx, provideroperationsmetadata.DefaultListOperationOptions())` can be used to do batched pagination
+items, err := client.ListComplete(ctx, provideroperationsmetadata.DefaultListOperationOptions())
+if err != nil {
+	// handle the error
+}
+for _, item := range items {
+	// do something
+}
+```
---
+++ resource-manager/authorization/2018-01-01-preview/roleassignments/id_roleid_test.go
@@ -0,0 +1,147 @@
+package roleassignments
+
+import (
+	"testing"
+
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/resourceids"
+)
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+var _ resourceids.ResourceId = RoleIdId{}
+
+func TestNewRoleIdID(t *testing.T) {
+	id := NewRoleIdID("roleIdValue")
+
+	if id.RoleId != "roleIdValue" {
+		t.Fatalf("Expected %q but got %q for Segment 'RoleId'", id.RoleId, "roleIdValue")
+	}
+}
+
+func TestFormatRoleIdID(t *testing.T) {
+	actual := NewRoleIdID("roleIdValue").ID()
+	expected := "/roleIdValue"
+	if actual != expected {
+		t.Fatalf("Expected the Formatted ID to be %q but got %q", expected, actual)
+	}
+}
+
+func TestParseRoleIdID(t *testing.T) {
+	testData := []struct {
+		Input    string
+		Error    bool
+		Expected *RoleIdId
+	}{
+		{
+			// Incomplete URI
+			Input: "",
+			Error: true,
+		},
+		{
+			// Valid URI
+			Input: "/roleIdValue",
+			Expected: &RoleIdId{
+				RoleId: "roleIdValue",
+			},
+		},
+		{
+			// Invalid (Valid Uri with Extra segment)
+			Input: "/roleIdValue/extra",
+			Error: true,
+		},
+	}
+	for _, v := range testData {
+		t.Logf("[DEBUG] Testing %q", v.Input)
+
+		actual, err := ParseRoleIdID(v.Input)
+		if err != nil {
+			if v.Error {
+				continue
+			}
+
+			t.Fatalf("Expect a value but got an error: %+v", err)
+		}
+		if v.Error {
+			t.Fatal("Expect an error but didn't get one")
+		}
+
+		if actual.RoleId != v.Expected.RoleId {
+			t.Fatalf("Expected %q but got %q for RoleId", v.Expected.RoleId, actual.RoleId)
+		}
+
+	}
+}
+
+func TestParseRoleIdIDInsensitively(t *testing.T) {
+	testData := []struct {
+		Input    string
+		Error    bool
+		Expected *RoleIdId
+	}{
+		{
+			// Incomplete URI
+			Input: "",
+			Error: true,
+		},
+		{
+			// Valid URI
+			Input: "/roleIdValue",
+			Expected: &RoleIdId{
+				RoleId: "roleIdValue",
+			},
+		},
+		{
+			// Invalid (Valid Uri with Extra segment)
+			Input: "/roleIdValue/extra",
+			Error: true,
+		},
+		{
+			// Valid URI (mIxEd CaSe since this is insensitive)
+			Input: "/rOlEiDvAlUe",
+			Expected: &RoleIdId{
+				RoleId: "rOlEiDvAlUe",
+			},
+		},
+		{
+			// Invalid (Valid Uri with Extra segment - mIxEd CaSe since this is insensitive)
+			Input: "/rOlEiDvAlUe/extra",
+			Error: true,
+		},
+	}
+	for _, v := range testData {
+		t.Logf("[DEBUG] Testing %q", v.Input)
+
+		actual, err := ParseRoleIdIDInsensitively(v.Input)
+		if err != nil {
+			if v.Error {
+				continue
+			}
+
+			t.Fatalf("Expect a value but got an error: %+v", err)
+		}
+		if v.Error {
+			t.Fatal("Expect an error but didn't get one")
+		}
+
+		if actual.RoleId != v.Expected.RoleId {
+			t.Fatalf("Expected %q but got %q for RoleId", v.Expected.RoleId, actual.RoleId)
+		}
+
+	}
+}
+
+func TestSegmentsForRoleIdId(t *testing.T) {
+	segments := RoleIdId{}.Segments()
+	if len(segments) == 0 {
+		t.Fatalf("RoleIdId has no segments")
+	}
+
+	uniqueNames := make(map[string]struct{}, 0)
+	for _, segment := range segments {
+		uniqueNames[segment.Name] = struct{}{}
+	}
+	if len(uniqueNames) != len(segments) {
+		t.Fatalf("Expected the Segments to be unique but got %q unique segments and %d total segments", len(uniqueNames), len(segments))
+	}
+}
