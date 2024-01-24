e910dc80b5e6c9bccb5344c825a5c28047d2e79d
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-01-24 17:56:17 +0100

    scripts: updating to support publishing/validating the new `resource-manager` submodule

--- scripts/publish-git-tag.sh
+++ scripts/publish-git-tag.sh
@@ -9,6 +9,9 @@ DIR="$(cd "$(dirname "$0")" && pwd)/.."
 function publish {
   local version=$1
 
+  echo "Publishing the branch (if required).."
+  git push
+
   echo "Tagging as '$version'.."
   git tag "$version"
 
@@ -16,13 +19,42 @@ function publish {
   git push --tags
 }
 
+function updateSdkReferenceAndCommitChanges {
+  local directory=$1
+  local tag=$2
+
+  cd "${directory}"
+
+  echo "Updating the dependency on 'github.com/hashicorp/go-azure-sdk/sdk'.."
+  go get "github.com/hashicorp/go-azure-sdk/sdk@$tag"
+
+  echo "Running 'go mod tidy'.."
+  go mod tidy
+
+  echo "Running 'go mod vendor'.."
+  go mod vendor
+
+  echo "Adding the updated go.mod/go.sum files.."
+  git add --all
+
+  echo "Committing the changes.."
+  git commit -m "$directory: updating to '$tag' of 'github.com/hashicorp/go-azure-sdk/sdk'"
+
+  cd "${DIR}"
+}
+
 function main {
   local version=$1
 
+  local resourceManagerTag="resource-manager/$version"
   local sdkTag="sdk/$version"
 
   echo "Releasing the Base Layer.."
   publish "$sdkTag"
+
+  echo "Releasing the Resource Manager SDK.."
+  updateSdkReferenceAndCommitChanges "resource-manager" "$version"
+  publish "$resourceManagerTag"
 }
 
 main "$1"
