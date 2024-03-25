ec0083f539696d6b9a2b4f07210b8847aa4b73e2
hc-github-team-tf-azure <>
2024-03-25 11:09:54 +0000

    Updating based on 4c47d693

--- resource-manager/containerinstance/2023-05-01/client.go
+++ resource-manager/containerinstance/2023-05-01/client.go
@@ -4,20 +4,25 @@ package v2023_05_01
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
 import (
-	"github.com/Azure/go-autorest/autorest"
+	"fmt"
+
 	"github.com/hashicorp/go-azure-sdk/resource-manager/containerinstance/2023-05-01/containerinstance"
+	"github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager"
+	sdkEnv "github.com/hashicorp/go-azure-sdk/sdk/environments"
 )
 
 type Client struct {
 	ContainerInstance *containerinstance.ContainerInstanceClient
 }
 
-func NewClientWithBaseURI(endpoint string, configureAuthFunc func(c *autorest.Client)) Client {
-
-	containerInstanceClient := containerinstance.NewContainerInstanceClientWithBaseURI(endpoint)
-	configureAuthFunc(&containerInstanceClient.Client)
-
-	return Client{
-		ContainerInstance: &containerInstanceClient,
+func NewClientWithBaseURI(sdkApi sdkEnv.Api, configureFunc func(c *resourcemanager.Client)) (*Client, error) {
+	containerInstanceClient, err := containerinstance.NewContainerInstanceClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building ContainerInstance client: %+v", err)
 	}
+	configureFunc(containerInstanceClient.Client)
+
+	return &Client{
+		ContainerInstance: containerInstanceClient,
+	}, nil
 }
