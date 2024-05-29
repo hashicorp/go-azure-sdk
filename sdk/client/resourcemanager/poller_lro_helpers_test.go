f3f8bb4cced4c87c927202cefa62214771b87b5c
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-05-29 18:06:34 +0200

    `client/resourcemanager`: updating the LRO to treat a 404 as InProgress for non-delete LROs
    Delete LROs have a unique poller, so this won't apply to those, and should fix the open issues (TBD)

---
+++ sdk/client/resourcemanager/poller_lro_helpers_test.go
@@ -0,0 +1,104 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package resourcemanager
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
+)
+
+// longRunningOperationsEndpoint is a test helper struct which makes testing the expected behaviour of the
+// long-running operations endpoint easier. Originally these started out as a function per test, but with
+// so much boilerplate, this helped make the tests more readable, so whilst this has some boilerplate I
+// suspect it's worth it for the tradeoff of having tests that are more understandable.
+type longRunningOperationsEndpoint struct {
+	numberOfTimesCalled  int
+	responses            []longRunningOperationsExpectedResponse
+	useProvisioningState bool
+}
+
+type longRunningOperationsExpectedResponse struct {
+	status         *status
+	httpStatusCode *int
+}
+
+func responseWithStatus(input status) longRunningOperationsExpectedResponse {
+	return longRunningOperationsExpectedResponse{
+		status: pointer.To(input),
+	}
+}
+
+func responseWithHttpStatusCode(input int) longRunningOperationsExpectedResponse {
+	return longRunningOperationsExpectedResponse{
+		httpStatusCode: pointer.To(input),
+	}
+}
+
+func newLongRunningOperationsEndpoint(useProvisioningState bool, responses []longRunningOperationsExpectedResponse) longRunningOperationsEndpoint {
+	return longRunningOperationsEndpoint{
+		useProvisioningState: useProvisioningState,
+		numberOfTimesCalled:  0,
+		responses:            responses,
+	}
+}
+
+func (e *longRunningOperationsEndpoint) endpoint(t *testing.T) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		t.Logf("Server request %q %q", r.Method, r.RequestURI)
+
+		if r.Method != http.MethodGet && r.RequestURI != "/lro/poll" {
+			w.WriteHeader(http.StatusNotImplemented)
+			return
+		}
+
+		e.numberOfTimesCalled++
+		if e.numberOfTimesCalled > len(e.responses) {
+			t.Fatal("called too many times")
+		}
+		response := e.responses[e.numberOfTimesCalled-1]
+		if response.httpStatusCode != nil {
+			w.WriteHeader(*response.httpStatusCode)
+			return
+		}
+
+		if response.status == nil {
+			t.Fatalf("missing either a httpStatusCode or a status to return")
+		}
+		payload := operationResult{
+			Properties: operationResultProperties{
+				ProvisioningState: "",
+			},
+			Status: "",
+		}
+		if e.useProvisioningState {
+			payload.Properties.ProvisioningState = *response.status
+		} else {
+			payload.Status = *response.status
+		}
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(payload); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			t.Fatalf(err.Error())
+			return
+		}
+	}
+}
+
+func (e longRunningOperationsEndpoint) assertCalled(t *testing.T, expected int) {
+	if e.numberOfTimesCalled != expected {
+		t.Fatalf("expected to be called %d times but got %d", expected, e.numberOfTimesCalled)
+	}
+}
+
+func (e longRunningOperationsEndpoint) response() *http.Response {
+	return &http.Response{
+		Header: map[string][]string{
+			http.CanonicalHeaderKey("Azure-AsyncOperation"): {"http://localhost/lro/poll"},
+			http.CanonicalHeaderKey("Retry-After"):          {"1"},
+		},
+	}
+}
