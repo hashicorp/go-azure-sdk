7b6c20f99948e4ec5f39bbd958a0f6d6b3df6e8c
hc-github-team-tf-azure <>
2024-06-05 15:29:28 +0000

    Updating based on b9eddbf3

--- resource-manager/storage/2023-05-01/blobcontainers/model_immutabilitypolicy.go
+++ resource-manager/storage/2023-05-01/blobcontainers/model_immutabilitypolicy.go
@@ -1,9 +1,5 @@
 package blobcontainers
 
-import (
-	"github.com/hashicorp/go-azure-helpers/resourcemanager/systemdata"
-)
-
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
@@ -12,6 +8,5 @@ type ImmutabilityPolicy struct {
 	Id         *string                    `json:"id,omitempty"`
 	Name       *string                    `json:"name,omitempty"`
 	Properties ImmutabilityPolicyProperty `json:"properties"`
-	SystemData *systemdata.SystemData     `json:"systemData,omitempty"`
 	Type       *string                    `json:"type,omitempty"`
 }
--- resource-manager/storage/2023-05-01/fileservice/model_fileserviceproperties.go
+++ resource-manager/storage/2023-05-01/fileservice/model_fileserviceproperties.go
@@ -1,9 +1,5 @@
 package fileservice
 
-import (
-	"github.com/hashicorp/go-azure-helpers/resourcemanager/systemdata"
-)
-
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
@@ -12,6 +8,5 @@ type FileServiceProperties struct {
 	Name       *string                          `json:"name,omitempty"`
 	Properties *FileServicePropertiesProperties `json:"properties,omitempty"`
 	Sku        *Sku                             `json:"sku,omitempty"`
-	SystemData *systemdata.SystemData           `json:"systemData,omitempty"`
 	Type       *string                          `json:"type,omitempty"`
 }
