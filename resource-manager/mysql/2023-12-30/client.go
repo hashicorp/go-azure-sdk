312752ed45149d79d0bf3f46a71dbdd18885f010
hc-github-team-tf-azure <>
2024-06-25 09:01:09 +0000

    Updating based on 490db768

--- resource-manager/mysql/2023-12-30/client.go
+++ resource-manager/mysql/2023-12-30/client.go
@@ -7,10 +7,19 @@ import (
 	"fmt"
 
 	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/advancedthreatprotectionsettings"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/azureadadministrators"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/backupandexport"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/backups"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/backupsv2"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/checknameavailability"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/configurations"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/databases"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/firewallrules"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/getprivatednszonesuffix"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/locationbasedcapabilities"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/locationbasedcapability"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/logfiles"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/maintenances"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/operationprogress"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/serverfailover"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/mysql/2023-12-30/servermigration"
@@ -26,10 +35,19 @@ import (
 
 type Client struct {
 	AdvancedThreatProtectionSettings       *advancedthreatprotectionsettings.AdvancedThreatProtectionSettingsClient
+	AzureADAdministrators                  *azureadadministrators.AzureADAdministratorsClient
+	BackupAndExport                        *backupandexport.BackupAndExportClient
+	Backups                                *backups.BackupsClient
+	BackupsV2                              *backupsv2.BackupsV2Client
 	CheckNameAvailability                  *checknameavailability.CheckNameAvailabilityClient
+	Configurations                         *configurations.ConfigurationsClient
+	Databases                              *databases.DatabasesClient
+	FirewallRules                          *firewallrules.FirewallRulesClient
 	GetPrivateDnsZoneSuffix                *getprivatednszonesuffix.GetPrivateDnsZoneSuffixClient
 	LocationBasedCapabilities              *locationbasedcapabilities.LocationBasedCapabilitiesClient
 	LocationBasedCapability                *locationbasedcapability.LocationBasedCapabilityClient
+	LogFiles                               *logfiles.LogFilesClient
+	Maintenances                           *maintenances.MaintenancesClient
 	OperationProgress                      *operationprogress.OperationProgressClient
 	ServerFailover                         *serverfailover.ServerFailoverClient
 	ServerMigration                        *servermigration.ServerMigrationClient
@@ -48,11 +66,53 @@ func NewClientWithBaseURI(sdkApi sdkEnv.Api, configureFunc func(c *resourcemanag
 	}
 	configureFunc(advancedThreatProtectionSettingsClient.Client)
 
+	azureADAdministratorsClient, err := azureadadministrators.NewAzureADAdministratorsClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building AzureADAdministrators client: %+v", err)
+	}
+	configureFunc(azureADAdministratorsClient.Client)
+
+	backupAndExportClient, err := backupandexport.NewBackupAndExportClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building BackupAndExport client: %+v", err)
+	}
+	configureFunc(backupAndExportClient.Client)
+
+	backupsClient, err := backups.NewBackupsClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building Backups client: %+v", err)
+	}
+	configureFunc(backupsClient.Client)
+
+	backupsV2Client, err := backupsv2.NewBackupsV2ClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building BackupsV2 client: %+v", err)
+	}
+	configureFunc(backupsV2Client.Client)
+
 	checkNameAvailabilityClient, err := checknameavailability.NewCheckNameAvailabilityClientWithBaseURI(sdkApi)
 	if err != nil {
 		return nil, fmt.Errorf("building CheckNameAvailability client: %+v", err)
 	}
 	configureFunc(checkNameAvailabilityClient.Client)
+
+	configurationsClient, err := configurations.NewConfigurationsClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building Configurations client: %+v", err)
+	}
+	configureFunc(configurationsClient.Client)
+
+	databasesClient, err := databases.NewDatabasesClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building Databases client: %+v", err)
+	}
+	configureFunc(databasesClient.Client)
+
+	firewallRulesClient, err := firewallrules.NewFirewallRulesClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building FirewallRules client: %+v", err)
+	}
+	configureFunc(firewallRulesClient.Client)
 
 	getPrivateDnsZoneSuffixClient, err := getprivatednszonesuffix.NewGetPrivateDnsZoneSuffixClientWithBaseURI(sdkApi)
 	if err != nil {
@@ -71,6 +131,18 @@ func NewClientWithBaseURI(sdkApi sdkEnv.Api, configureFunc func(c *resourcemanag
 		return nil, fmt.Errorf("building LocationBasedCapability client: %+v", err)
 	}
 	configureFunc(locationBasedCapabilityClient.Client)
+
+	logFilesClient, err := logfiles.NewLogFilesClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building LogFiles client: %+v", err)
+	}
+	configureFunc(logFilesClient.Client)
+
+	maintenancesClient, err := maintenances.NewMaintenancesClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building Maintenances client: %+v", err)
+	}
+	configureFunc(maintenancesClient.Client)
 
 	operationProgressClient, err := operationprogress.NewOperationProgressClientWithBaseURI(sdkApi)
 	if err != nil {
@@ -128,10 +200,19 @@ func NewClientWithBaseURI(sdkApi sdkEnv.Api, configureFunc func(c *resourcemanag
 
 	return &Client{
 		AdvancedThreatProtectionSettings:       advancedThreatProtectionSettingsClient,
+		AzureADAdministrators:                  azureADAdministratorsClient,
+		BackupAndExport:                        backupAndExportClient,
+		Backups:                                backupsClient,
+		BackupsV2:                              backupsV2Client,
 		CheckNameAvailability:                  checkNameAvailabilityClient,
+		Configurations:                         configurationsClient,
+		Databases:                              databasesClient,
+		FirewallRules:                          firewallRulesClient,
 		GetPrivateDnsZoneSuffix:                getPrivateDnsZoneSuffixClient,
 		LocationBasedCapabilities:              locationBasedCapabilitiesClient,
 		LocationBasedCapability:                locationBasedCapabilityClient,
+		LogFiles:                               logFilesClient,
+		Maintenances:                           maintenancesClient,
 		OperationProgress:                      operationProgressClient,
 		ServerFailover:                         serverFailoverClient,
 		ServerMigration:                        serverMigrationClient,
