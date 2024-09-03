851b7ea0fd445f15eaded7bd7a8796d17fa409b9
hc-github-team-tf-azure <>
2024-09-03 11:25:03 +0000

    Updating based on 3e9a4435

---
+++ resource-manager/datafactory/2018-06-01/pipelines/model_icebergwritesettings.go
@@ -0,0 +1,40 @@
+package pipelines
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+var _ FormatWriteSettings = IcebergWriteSettings{}
+
+type IcebergWriteSettings struct {
+
+	// Fields inherited from FormatWriteSettings
+}
+
+var _ json.Marshaler = IcebergWriteSettings{}
+
+func (s IcebergWriteSettings) MarshalJSON() ([]byte, error) {
+	type wrapper IcebergWriteSettings
+	wrapped := wrapper(s)
+	encoded, err := json.Marshal(wrapped)
+	if err != nil {
+		return nil, fmt.Errorf("marshaling IcebergWriteSettings: %+v", err)
+	}
+
+	var decoded map[string]interface{}
+	if err := json.Unmarshal(encoded, &decoded); err != nil {
+		return nil, fmt.Errorf("unmarshaling IcebergWriteSettings: %+v", err)
+	}
+	decoded["type"] = "IcebergWriteSettings"
+
+	encoded, err = json.Marshal(decoded)
+	if err != nil {
+		return nil, fmt.Errorf("re-marshaling IcebergWriteSettings: %+v", err)
+	}
+
+	return encoded, nil
+}
