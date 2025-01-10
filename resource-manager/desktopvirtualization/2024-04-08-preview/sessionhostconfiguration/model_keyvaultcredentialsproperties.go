8d20b7e2c5e4839a9e9d70b368ad8dd5057934f0
hc-github-team-tf-azure <>
2025-01-10 07:04:39 +0000

    Updating based on e223c8a34

--- resource-manager/desktopvirtualization/2024-04-08-preview/sessionhostconfiguration/model_keyvaultcredentialsproperties.go
+++
@@ -1,9 +0,0 @@
-package sessionhostconfiguration
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type KeyVaultCredentialsProperties struct {
-	PasswordKeyVaultSecretUri string `json:"passwordKeyVaultSecretUri"`
-	UsernameKeyVaultSecretUri string `json:"usernameKeyVaultSecretUri"`
-}
