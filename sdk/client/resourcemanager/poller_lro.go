259110af53e622b46c582f1abf23cab45d8f6359
magodo <wztwcy@gmail.com>
2025-07-23 18:32:08 +1000

    lro return

--- sdk/client/resourcemanager/poller_lro.go
+++ sdk/client/resourcemanager/poller_lro.go
@@ -184,6 +184,7 @@ func (p *longRunningOperationPoller) Poll(ctx context.Context) (result *pollers.
 				HttpResponse: result.HttpResponse,
 				Message:      lroError.Error(),
 			}
+			return
 		}
 
 		if result.Status == pollers.PollingStatusCancelled || op.Status == statusCanceled || op.Properties.ProvisioningState == statusCanceled {
@@ -196,6 +197,7 @@ func (p *longRunningOperationPoller) Poll(ctx context.Context) (result *pollers.
 				HttpResponse: result.HttpResponse,
 				Message:      lroError.Error(),
 			}
+			return
 		}
 
 		if result.Status == pollers.PollingStatusSucceeded || op.Status == statusSucceeded || op.Properties.ProvisioningState == statusSucceeded {
