7700efebc9470b4b2d42f0da8cf12e969eb38522
hc-github-team-tf-azure <>
2025-09-24 21:59:27 +0000

    Updating based on 6b2cde998

---
+++ resource-manager/iotoperations/2024-11-01/brokerlistener/id_listener_test.go
@@ -0,0 +1,372 @@
+package brokerlistener
+
+import (
+	"testing"
+
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/resourceids"
+)
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+var _ resourceids.ResourceId = &ListenerId{}
+
+func TestNewListenerID(t *testing.T) {
+	id := NewListenerID("12345678-1234-9876-4563-123456789012", "example-resource-group", "instanceName", "brokerName", "listenerName")
+
+	if id.SubscriptionId != "12345678-1234-9876-4563-123456789012" {
+		t.Fatalf("Expected %q but got %q for Segment 'SubscriptionId'", id.SubscriptionId, "12345678-1234-9876-4563-123456789012")
+	}
+
+	if id.ResourceGroupName != "example-resource-group" {
+		t.Fatalf("Expected %q but got %q for Segment 'ResourceGroupName'", id.ResourceGroupName, "example-resource-group")
+	}
+
+	if id.InstanceName != "instanceName" {
+		t.Fatalf("Expected %q but got %q for Segment 'InstanceName'", id.InstanceName, "instanceName")
+	}
+
+	if id.BrokerName != "brokerName" {
+		t.Fatalf("Expected %q but got %q for Segment 'BrokerName'", id.BrokerName, "brokerName")
+	}
+
+	if id.ListenerName != "listenerName" {
+		t.Fatalf("Expected %q but got %q for Segment 'ListenerName'", id.ListenerName, "listenerName")
+	}
+}
+
+func TestFormatListenerID(t *testing.T) {
+	actual := NewListenerID("12345678-1234-9876-4563-123456789012", "example-resource-group", "instanceName", "brokerName", "listenerName").ID()
+	expected := "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName/brokers/brokerName/listeners/listenerName"
+	if actual != expected {
+		t.Fatalf("Expected the Formatted ID to be %q but got %q", expected, actual)
+	}
+}
+
+func TestParseListenerID(t *testing.T) {
+	testData := []struct {
+		Input    string
+		Error    bool
+		Expected *ListenerId
+	}{
+		{
+			// Incomplete URI
+			Input: "",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName/brokers",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName/brokers/brokerName",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName/brokers/brokerName/listeners",
+			Error: true,
+		},
+		{
+			// Valid URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName/brokers/brokerName/listeners/listenerName",
+			Expected: &ListenerId{
+				SubscriptionId:    "12345678-1234-9876-4563-123456789012",
+				ResourceGroupName: "example-resource-group",
+				InstanceName:      "instanceName",
+				BrokerName:        "brokerName",
+				ListenerName:      "listenerName",
+			},
+		},
+		{
+			// Invalid (Valid Uri with Extra segment)
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName/brokers/brokerName/listeners/listenerName/extra",
+			Error: true,
+		},
+	}
+	for _, v := range testData {
+		t.Logf("[DEBUG] Testing %q", v.Input)
+
+		actual, err := ParseListenerID(v.Input)
+		if err != nil {
+			if v.Error {
+				continue
+			}
+
+			t.Fatalf("Expect a value but got an error: %+v", err)
+		}
+		if v.Error {
+			t.Fatal("Expect an error but didn't get one")
+		}
+
+		if actual.SubscriptionId != v.Expected.SubscriptionId {
+			t.Fatalf("Expected %q but got %q for SubscriptionId", v.Expected.SubscriptionId, actual.SubscriptionId)
+		}
+
+		if actual.ResourceGroupName != v.Expected.ResourceGroupName {
+			t.Fatalf("Expected %q but got %q for ResourceGroupName", v.Expected.ResourceGroupName, actual.ResourceGroupName)
+		}
+
+		if actual.InstanceName != v.Expected.InstanceName {
+			t.Fatalf("Expected %q but got %q for InstanceName", v.Expected.InstanceName, actual.InstanceName)
+		}
+
+		if actual.BrokerName != v.Expected.BrokerName {
+			t.Fatalf("Expected %q but got %q for BrokerName", v.Expected.BrokerName, actual.BrokerName)
+		}
+
+		if actual.ListenerName != v.Expected.ListenerName {
+			t.Fatalf("Expected %q but got %q for ListenerName", v.Expected.ListenerName, actual.ListenerName)
+		}
+
+	}
+}
+
+func TestParseListenerIDInsensitively(t *testing.T) {
+	testData := []struct {
+		Input    string
+		Error    bool
+		Expected *ListenerId
+	}{
+		{
+			// Incomplete URI
+			Input: "",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions",
+			Error: true,
+		},
+		{
+			// Incomplete URI (mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012",
+			Error: true,
+		},
+		{
+			// Incomplete URI (mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs/12345678-1234-9876-4563-123456789012",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups",
+			Error: true,
+		},
+		{
+			// Incomplete URI (mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs/12345678-1234-9876-4563-123456789012/rEsOuRcEgRoUpS",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group",
+			Error: true,
+		},
+		{
+			// Incomplete URI (mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs/12345678-1234-9876-4563-123456789012/rEsOuRcEgRoUpS/eXaMpLe-rEsOuRcE-GrOuP",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers",
+			Error: true,
+		},
+		{
+			// Incomplete URI (mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs/12345678-1234-9876-4563-123456789012/rEsOuRcEgRoUpS/eXaMpLe-rEsOuRcE-GrOuP/pRoViDeRs",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations",
+			Error: true,
+		},
+		{
+			// Incomplete URI (mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs/12345678-1234-9876-4563-123456789012/rEsOuRcEgRoUpS/eXaMpLe-rEsOuRcE-GrOuP/pRoViDeRs/mIcRoSoFt.iOtOpErAtIoNs",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances",
+			Error: true,
+		},
+		{
+			// Incomplete URI (mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs/12345678-1234-9876-4563-123456789012/rEsOuRcEgRoUpS/eXaMpLe-rEsOuRcE-GrOuP/pRoViDeRs/mIcRoSoFt.iOtOpErAtIoNs/iNsTaNcEs",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName",
+			Error: true,
+		},
+		{
+			// Incomplete URI (mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs/12345678-1234-9876-4563-123456789012/rEsOuRcEgRoUpS/eXaMpLe-rEsOuRcE-GrOuP/pRoViDeRs/mIcRoSoFt.iOtOpErAtIoNs/iNsTaNcEs/iNsTaNcEnAmE",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName/brokers",
+			Error: true,
+		},
+		{
+			// Incomplete URI (mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs/12345678-1234-9876-4563-123456789012/rEsOuRcEgRoUpS/eXaMpLe-rEsOuRcE-GrOuP/pRoViDeRs/mIcRoSoFt.iOtOpErAtIoNs/iNsTaNcEs/iNsTaNcEnAmE/bRoKeRs",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName/brokers/brokerName",
+			Error: true,
+		},
+		{
+			// Incomplete URI (mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs/12345678-1234-9876-4563-123456789012/rEsOuRcEgRoUpS/eXaMpLe-rEsOuRcE-GrOuP/pRoViDeRs/mIcRoSoFt.iOtOpErAtIoNs/iNsTaNcEs/iNsTaNcEnAmE/bRoKeRs/bRoKeRnAmE",
+			Error: true,
+		},
+		{
+			// Incomplete URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName/brokers/brokerName/listeners",
+			Error: true,
+		},
+		{
+			// Incomplete URI (mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs/12345678-1234-9876-4563-123456789012/rEsOuRcEgRoUpS/eXaMpLe-rEsOuRcE-GrOuP/pRoViDeRs/mIcRoSoFt.iOtOpErAtIoNs/iNsTaNcEs/iNsTaNcEnAmE/bRoKeRs/bRoKeRnAmE/lIsTeNeRs",
+			Error: true,
+		},
+		{
+			// Valid URI
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName/brokers/brokerName/listeners/listenerName",
+			Expected: &ListenerId{
+				SubscriptionId:    "12345678-1234-9876-4563-123456789012",
+				ResourceGroupName: "example-resource-group",
+				InstanceName:      "instanceName",
+				BrokerName:        "brokerName",
+				ListenerName:      "listenerName",
+			},
+		},
+		{
+			// Invalid (Valid Uri with Extra segment)
+			Input: "/subscriptions/12345678-1234-9876-4563-123456789012/resourceGroups/example-resource-group/providers/Microsoft.IoTOperations/instances/instanceName/brokers/brokerName/listeners/listenerName/extra",
+			Error: true,
+		},
+		{
+			// Valid URI (mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs/12345678-1234-9876-4563-123456789012/rEsOuRcEgRoUpS/eXaMpLe-rEsOuRcE-GrOuP/pRoViDeRs/mIcRoSoFt.iOtOpErAtIoNs/iNsTaNcEs/iNsTaNcEnAmE/bRoKeRs/bRoKeRnAmE/lIsTeNeRs/lIsTeNeRnAmE",
+			Expected: &ListenerId{
+				SubscriptionId:    "12345678-1234-9876-4563-123456789012",
+				ResourceGroupName: "eXaMpLe-rEsOuRcE-GrOuP",
+				InstanceName:      "iNsTaNcEnAmE",
+				BrokerName:        "bRoKeRnAmE",
+				ListenerName:      "lIsTeNeRnAmE",
+			},
+		},
+		{
+			// Invalid (Valid Uri with Extra segment - mIxEd CaSe since this is insensitive)
+			Input: "/sUbScRiPtIoNs/12345678-1234-9876-4563-123456789012/rEsOuRcEgRoUpS/eXaMpLe-rEsOuRcE-GrOuP/pRoViDeRs/mIcRoSoFt.iOtOpErAtIoNs/iNsTaNcEs/iNsTaNcEnAmE/bRoKeRs/bRoKeRnAmE/lIsTeNeRs/lIsTeNeRnAmE/extra",
+			Error: true,
+		},
+	}
+	for _, v := range testData {
+		t.Logf("[DEBUG] Testing %q", v.Input)
+
+		actual, err := ParseListenerIDInsensitively(v.Input)
+		if err != nil {
+			if v.Error {
+				continue
+			}
+
+			t.Fatalf("Expect a value but got an error: %+v", err)
+		}
+		if v.Error {
+			t.Fatal("Expect an error but didn't get one")
+		}
+
+		if actual.SubscriptionId != v.Expected.SubscriptionId {
+			t.Fatalf("Expected %q but got %q for SubscriptionId", v.Expected.SubscriptionId, actual.SubscriptionId)
+		}
+
+		if actual.ResourceGroupName != v.Expected.ResourceGroupName {
+			t.Fatalf("Expected %q but got %q for ResourceGroupName", v.Expected.ResourceGroupName, actual.ResourceGroupName)
+		}
+
+		if actual.InstanceName != v.Expected.InstanceName {
+			t.Fatalf("Expected %q but got %q for InstanceName", v.Expected.InstanceName, actual.InstanceName)
+		}
+
+		if actual.BrokerName != v.Expected.BrokerName {
+			t.Fatalf("Expected %q but got %q for BrokerName", v.Expected.BrokerName, actual.BrokerName)
+		}
+
+		if actual.ListenerName != v.Expected.ListenerName {
+			t.Fatalf("Expected %q but got %q for ListenerName", v.Expected.ListenerName, actual.ListenerName)
+		}
+
+	}
+}
+
+func TestSegmentsForListenerId(t *testing.T) {
+	segments := ListenerId{}.Segments()
+	if len(segments) == 0 {
+		t.Fatalf("ListenerId has no segments")
+	}
+
+	uniqueNames := make(map[string]struct{}, 0)
+	for _, segment := range segments {
+		uniqueNames[segment.Name] = struct{}{}
+	}
+	if len(uniqueNames) != len(segments) {
+		t.Fatalf("Expected the Segments to be unique but got %q unique segments and %d total segments", len(uniqueNames), len(segments))
+	}
+}
---
+++ resource-manager/iotoperations/2024-11-01/client.go
@@ -0,0 +1,91 @@
+package v2024_11_01
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/go-azure-sdk/resource-manager/iotoperations/2024-11-01/broker"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/iotoperations/2024-11-01/brokerauthentication"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/iotoperations/2024-11-01/brokerauthorization"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/iotoperations/2024-11-01/brokerlistener"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/iotoperations/2024-11-01/dataflow"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/iotoperations/2024-11-01/dataflowendpoint"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/iotoperations/2024-11-01/dataflowprofile"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/iotoperations/2024-11-01/instance"
+	"github.com/hashicorp/go-azure-sdk/sdk/client/resourcemanager"
+	sdkEnv "github.com/hashicorp/go-azure-sdk/sdk/environments"
+)
+
+type Client struct {
+	Broker               *broker.BrokerClient
+	BrokerAuthentication *brokerauthentication.BrokerAuthenticationClient
+	BrokerAuthorization  *brokerauthorization.BrokerAuthorizationClient
+	BrokerListener       *brokerlistener.BrokerListenerClient
+	Dataflow             *dataflow.DataflowClient
+	DataflowEndpoint     *dataflowendpoint.DataflowEndpointClient
+	DataflowProfile      *dataflowprofile.DataflowProfileClient
+	Instance             *instance.InstanceClient
+}
+
+func NewClientWithBaseURI(sdkApi sdkEnv.Api, configureFunc func(c *resourcemanager.Client)) (*Client, error) {
+	brokerAuthenticationClient, err := brokerauthentication.NewBrokerAuthenticationClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building BrokerAuthentication client: %+v", err)
+	}
+	configureFunc(brokerAuthenticationClient.Client)
+
+	brokerAuthorizationClient, err := brokerauthorization.NewBrokerAuthorizationClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building BrokerAuthorization client: %+v", err)
+	}
+	configureFunc(brokerAuthorizationClient.Client)
+
+	brokerClient, err := broker.NewBrokerClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building Broker client: %+v", err)
+	}
+	configureFunc(brokerClient.Client)
+
+	brokerListenerClient, err := brokerlistener.NewBrokerListenerClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building BrokerListener client: %+v", err)
+	}
+	configureFunc(brokerListenerClient.Client)
+
+	dataflowClient, err := dataflow.NewDataflowClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building Dataflow client: %+v", err)
+	}
+	configureFunc(dataflowClient.Client)
+
+	dataflowEndpointClient, err := dataflowendpoint.NewDataflowEndpointClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building DataflowEndpoint client: %+v", err)
+	}
+	configureFunc(dataflowEndpointClient.Client)
+
+	dataflowProfileClient, err := dataflowprofile.NewDataflowProfileClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building DataflowProfile client: %+v", err)
+	}
+	configureFunc(dataflowProfileClient.Client)
+
+	instanceClient, err := instance.NewInstanceClientWithBaseURI(sdkApi)
+	if err != nil {
+		return nil, fmt.Errorf("building Instance client: %+v", err)
+	}
+	configureFunc(instanceClient.Client)
+
+	return &Client{
+		Broker:               brokerClient,
+		BrokerAuthentication: brokerAuthenticationClient,
+		BrokerAuthorization:  brokerAuthorizationClient,
+		BrokerListener:       brokerListenerClient,
+		Dataflow:             dataflowClient,
+		DataflowEndpoint:     dataflowEndpointClient,
+		DataflowProfile:      dataflowProfileClient,
+		Instance:             instanceClient,
+	}, nil
+}
