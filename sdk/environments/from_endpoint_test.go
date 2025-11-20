013578e1069e74ec910aa7ed8dd9bf5893051063
Sam Mauck <sammauck@microsoft.com>
2025-11-20 14:15:23 -0800

    Sanitize resourceManager endpoint to work around ARM metadata service inconsistencies

--- sdk/environments/from_endpoint_test.go
+++ sdk/environments/from_endpoint_test.go
@@ -17,6 +17,14 @@ func TestFromEndpoint_Public(t *testing.T) {
 	if actual.Name != "AzureCloud" {
 		t.Fatalf("expected the Environment name to be `AzureCloud` but got %q", actual.Name)
 	}
+
+	endpoint, ok := actual.ResourceManager.Endpoint()
+	if !ok {
+		t.Fatalf("refreshing MetaData from Endpoint: no `ResourceManager` endpoint was defined")
+	}
+	if *endpoint != "https://management.azure.com" {
+		t.Fatalf("expected the ResourceManager endpoint to be `https://management.azure.com` but got %q", *endpoint)
+	}
 }
 
 func TestFromEndpoint_USGovernment(t *testing.T) {
@@ -27,5 +35,13 @@ func TestFromEndpoint_USGovernment(t *testing.T) {
 
 	if actual.Name != "AzureUSGovernment" {
 		t.Fatalf("expected the Environment name to be `AzureUSGovernment` but got %q", actual.Name)
+	}
+
+	endpoint, ok := actual.ResourceManager.Endpoint()
+	if !ok {
+		t.Fatalf("refreshing MetaData from Endpoint: no `ResourceManager` endpoint was defined")
+	}
+	if *endpoint != "https://management.usgovcloudapi.net" {
+		t.Fatalf("expected the ResourceManager endpoint to be `https://management.usgovcloudapi.net` but got %q", *endpoint)
 	}
 }
