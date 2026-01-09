c67196ae2eba031c262e7db094fe51f686cd6e7e
hc-github-team-tf-azure <>
2026-01-09 19:27:25 +0000

    Updating based on 4be1511e9

--- resource-manager/durabletask/2025-11-01/retentionpolicies/version.go
+++ resource-manager/durabletask/2025-11-01/retentionpolicies/version.go
@@ -1,9 +1,10 @@
-package protectioncontainers
+package retentionpolicies
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
-type ErrorAdditionalInfo struct {
-	Info *interface{} `json:"info,omitempty"`
-	Type *string      `json:"type,omitempty"`
+const defaultApiVersion = "2025-11-01"
+
+func userAgent() string {
+	return "hashicorp/go-azure-sdk/retentionpolicies/2025-11-01"
 }
