bbdf941ae897e6c8a1764acd2b4d2daede2db351
jackofallops <ste@hashicorp.com>
2026-01-20 17:21:21 +0100

    data-plane modifications

---
+++ data-plane/go.mod
@@ -0,0 +1,22 @@
+module github.com/hashicorp/go-azure-sdk/data-plane
+
+go 1.25.5
+
+require (
+	github.com/hashicorp/go-azure-helpers v0.73.0
+	github.com/hashicorp/go-azure-sdk/sdk v0.20260109.1234209
+)
+
+require (
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/hashicorp/go-version v1.7.0 // indirect
+	golang.org/x/crypto v0.45.0 // indirect
+	golang.org/x/oauth2 v0.30.0 // indirect
+	software.sslmate.com/src/go-pkcs12 v0.4.0 // indirect
+)
+
+replace github.com/hashicorp/go-azure-sdk/sdk => ../sdk
+
+replace github.com/hashicorp/go-azure-helpers => ../../go-azure-helpers
---
+++ sdk/client/dataplane/authorization_test.go
@@ -0,0 +1,39 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package dataplane_test
+
+import (
+	"context"
+	"net/http"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/go-azure-sdk/sdk/client/dataplane"
+	"github.com/hashicorp/go-azure-sdk/sdk/internal/test"
+)
+
+func TestAuthorizeResourceManagerRequest(t *testing.T) {
+	req := &http.Request{}
+	authorizer := &test.TestAuthorizer{}
+
+	err := dataplane.AuthorizeDataPlaneRequest(context.Background(), req, authorizer)
+	if err != nil {
+		t.Fatalf("received error: %+v", err)
+	}
+
+	expected := regexp.MustCompile("^Bearer [a-zA-Z0-9_-]+[.][a-zA-Z0-9_-]+[.][a-zA-Z0-9_-]+")
+	if val := req.Header.Get("Authorization"); !expected.MatchString(val) {
+		t.Fatalf("Authorization header mismatch, received: %q", val)
+	}
+	auxVals := strings.Split(req.Header.Get("X-Ms-Authorization-Auxiliary"), ",")
+	if len(auxVals) != 3 {
+		t.Fatalf("X-Ms-Authorization-Auxiliary mismatch, should contain 3 tokens, received: %q", strings.Join(auxVals, ","))
+	}
+	for i, auxVal := range auxVals {
+		if !expected.MatchString(strings.TrimSpace(auxVal)) {
+			t.Fatalf("Authorization header mismatch for token %d, received: %q", i, auxVal)
+		}
+	}
+}
