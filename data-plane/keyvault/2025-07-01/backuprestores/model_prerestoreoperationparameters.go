a159a2a430399a5bd86d48377bd965b34a55219e
hc-github-team-tf-azure <>
2026-06-02 13:57:48 +0000

    Updating based on 01687bd71

--- data-plane/keyvault/2025-07-01/backuprestores/model_prerestoreoperationparameters.go
+++
@@ -1,9 +0,0 @@
-package backuprestores
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type PreRestoreOperationParameters struct {
-	FolderToRestore    *string            `json:"folderToRestore,omitempty"`
-	SasTokenParameters *SASTokenParameter `json:"sasTokenParameters,omitempty"`
-}
